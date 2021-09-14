09a249f575a98e68804fec45e14cfabaacd4f3be
RoodyWang <roodywang@boyu66.net>
2021-09-14 14:00:39 +0800

    add problem

---
+++ LeetCode/Solution046.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class Solution046
+    {
+        public IList<IList<int>> Permute_NonRecursive(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>() { new List<int>() { nums[0] } };
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                IList<IList<int>> stepResult = new List<IList<int>>();
+                foreach (var item in result)
+                {
+                    for (var index = 0; index <= item.Count; index++)
+                    {
+                        var temp = new List<int>();
+                        temp.AddRange(item);
+                        temp.Insert(index,
+                            nums[i]);
+                        stepResult.Add(temp);
+                    }
+                }
+
+                result = stepResult;
+            }
+
+            return result;
+        }
+
+        public IList<IList<int>> Permute_Recursive(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>() { };
+            var unused = new List<int>(nums);
+
+            FindPermute(result,
+                new List<int>(),
+                unused);
+
+            return result;
+        }
+
+        private void FindPermute(IList<IList<int>> result,
+            List<int> curr,
+            List<int> unused)
+        {
+            if (unused.Count == 0)
+            {
+                result.Add(new List<int>(curr));
+            }
+
+            for (var i = 0; i < unused.Count; i++)
+            {
+                curr.Add(unused[i]);
+                unused.RemoveAt(i);
+                FindPermute(result,
+                    curr,
+                    unused);
+                unused.Insert(i,
+                    curr[curr.Count - 1]);
+                curr.RemoveAt(curr.Count - 1);
+            }
+        }
+    }
+}
---
+++ LeetCodeTests/Solution046Test.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Solution046Test
+    {
+        [TestCaseSource(nameof(TestCases))]
+        public void Permute_NonRecursiveTest(int[] input,
+            int[][] expectedArray)
+        {
+            var solution046 = new Solution046();
+            var expectList = new List<List<int>>();
+            foreach (var array in expectedArray)
+            {
+                expectList.Add(new List<int>(array));
+            }
+
+            var actual = solution046.Permute_NonRecursive(input);
+
+            actual.Should().BeEquivalentTo(expectList);
+        }
+        
+        [TestCaseSource(nameof(TestCases))]
+        public void Permute_RecursiveTest(int[] input,
+            int[][] expectedArray)
+        {
+            var solution046 = new Solution046();
+            var expectList = new List<List<int>>();
+            foreach (var array in expectedArray)
+            {
+                expectList.Add(new List<int>(array));
+            }
+
+            var actual = solution046.Permute_Recursive(input);
+
+            actual.Should().BeEquivalentTo(expectList);
+        }
+
+        private static readonly object[] TestCases =
+        {
+            new object[]
+            {
+                new int[] { 0 },
+                new int[][] { new int[] { 0 } }
+            },
+            new object[]
+            {
+                new int[] { 0, 1 },
+                new int[][] { new int[] { 0, 1 }, new int[] { 1, 0 } }
+            },
+            new object[]
+            {
+                new int[] { 0, 1, 2 },
+                new int[][]
+                {
+                    new int[] { 2, 0, 1 }, new int[] { 0, 2, 1 }, new int[] { 0, 1, 2 },
+                    new int[] { 2, 1, 0 }, new int[] { 1, 2, 0 }, new int[] { 1, 0, 2 }
+                }
+            }
+        };
+    }
+}

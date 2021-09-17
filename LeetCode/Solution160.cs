4d3053a90522f857da0cdfdd4ae7ec8daffda80a
ThunderstormWang <thunderstormwang@gmail.com>
2021-09-17 11:09:47 +0800

    refactor

--- LeetCode/Solution160.cs
+++ LeetCode/Solution160.cs
@@ -1,28 +1,79 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.NetworkInformation;
 
 namespace LeetCode
 {
     public class Solution160
     {
-        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        public ListNode GetIntersectionNode(ListNode headA,
+            ListNode headB)
         {
-            var headOfA = headA;
-            while (headOfA != null)
+            var dummyA = new ListNode(0);
+            dummyA.next = headA;
+            
+            var dummyB = new ListNode(0);
+            dummyB.next = headB;
+            
+            var traverseA = dummyA;
+            var traverseB = dummyB;
+
+            var lengthA = 0;
+            var lengthB = 0;
+            
+            while (traverseA.next != null )
             {
-                var headOfB = headB;
-                while (headOfB != null)
+                traverseA = traverseA.next;
+                lengthA++;
+            }
+            
+            while (traverseB.next != null )
+            {
+                traverseB = traverseB.next;
+                lengthB++;
+            }
+
+            if (lengthA > lengthB)
+            {
+                var diff = 0;
+                traverseB = dummyA.next;
+                while (diff < Math.Abs(lengthA - lengthB))
                 {
-                    if (headOfA == headOfB)
-                    {
-                        return headOfA;
-                    }
-                    headOfB = headOfB.next;
+                    traverseB = traverseB.next;
+                    diff++;
                 }
 
-                headOfA = headOfA.next;
-            } 
-            
+                traverseA = dummyB.next;
+            }
+            else if(lengthA < lengthB)
+            {
+                var diff = 0;
+                traverseA = dummyB.next;
+                while (diff < Math.Abs(lengthA - lengthB))
+                {
+                    traverseA = traverseA.next;
+                    diff++;
+                }
+                
+                traverseB = dummyA.next;
+            }
+            else
+            {
+                traverseA = dummyB.next;
+                traverseB = dummyA.next;
+            }
+
+            while (traverseA != null && traverseB != null)
+            {
+                if (traverseA == traverseB)
+                {
+                    return traverseA;
+                }
+
+                traverseA = traverseA.next;
+                traverseB = traverseB.next;
+            }
+
             return null;
         }
     }
--- LeetCodeTests/Solution160Test.cs
+++ LeetCodeTests/Solution160Test.cs
@@ -7,22 +7,44 @@ namespace LeetCodeTests
     [TestFixture]
     public class Solution160Test
     {
-        [TestCase(new int[] {4, 1}, new int[] {5, 6, 1}, new int[] {8, 4, 5})]
-        [TestCase(new int[] {1, 9, 1}, new int[] {3}, new int[] {2, 4})]
-        [TestCase(new int[] {2, 6, 4}, new int[] {1, 5}, new int[] { })]
-        public void GetIntersectionNodeTest(int[] arrayA, int[] arrayB, int[] intersections)
+        [TestCase(new int[] { 4, 1 },
+            new int[] { 5, 6, 1 },
+            new int[] { 8, 4, 5 })]
+        [TestCase(new int[] { 1, 9, 1 },
+            new int[] { 3 },
+            new int[] { 2, 4 })]
+        [TestCase(new int[] { 2, 6, 4 },
+            new int[] { 1, 5 },
+            new int[] { })]
+        [TestCase(new int[] { 2, 4, 6 },
+            new int[] { 1, 5 },
+            new int[] { 2, 4, 6 })]
+        [TestCase(new int[] { },
+            new int[] { 1, 5 },
+            new int[] { })]
+        [TestCase(new int[] { },
+            new int[] { },
+            new int[] { })]
+        public void GetIntersectionNodeTest(int[] arrayA,
+            int[] arrayB,
+            int[] intersections)
         {
-            var intersection = GetLinkedList(intersections, null);
-            var listA = GetLinkedList(arrayA, intersection);
-            var listB = GetLinkedList(arrayB, intersection);
+            var intersection = GetLinkedList(intersections,
+                null);
+            var listA = GetLinkedList(arrayA,
+                intersection);
+            var listB = GetLinkedList(arrayB,
+                intersection);
 
             var solution160 = new Solution160();
-            var actual = solution160.GetIntersectionNode(listA, listB);
+            var actual = solution160.GetIntersectionNode(listA,
+                listB);
 
             actual.Should().Be(intersection);
         }
 
-        private ListNode GetLinkedList(int[] input, in ListNode tail)
+        private ListNode GetLinkedList(int[] input,
+            in ListNode tail)
         {
             ListNode result = tail;
 

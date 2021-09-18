5623a71b52e16e857395aa330abef5beb6f91fc2
ThunderstormWang <thunderstormwang@gmail.com>
2021-09-18 21:13:14 +0800

    add problem

---
+++ LeetCode/Solution217.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class Solution217
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            var hashSet = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                if (!hashSet.Add(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCodeTests/Solution217Test.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Solution217Test
+    {
+        [TestCase(new int[] { 1, 2, 3, 1 },
+            true)]
+        [TestCase(new int[] { 1, 2, 3, 4 },
+            false)]
+        [TestCase(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 },
+            true)]
+        [TestCase(new int[] { },
+            false)]
+        [TestCase(new int[] { 999 },
+            false)]
+        public void ContainsDuplicateTest(int[] input,
+            bool expected)
+        {
+            var solution217 = new Solution217();
+            var actual = solution217.ContainsDuplicate(input);
+
+            actual.Should().Be(expected);
+        }
+    }
+}

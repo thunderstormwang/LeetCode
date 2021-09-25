0ddac5ad5b8c58c4d36bf4085b562994aa727d62
ThunderstormWang <thunderstormwang@gmail.com>
2021-09-25 12:10:51 +0800

    add problem 191

---
+++ LeetCodeTests/Solution191Test.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Solution191Test
+    {
+        [TestCase(0U,
+            0)]
+        [TestCase(11U,
+            3)]
+        [TestCase(128U,
+            1)]
+        [TestCase(4294967290U,
+            30)]
+        [TestCase(4294967293U,
+            31)]
+        [TestCase(4294967295U,
+            32)]
+        public void HammingWeight_ShiftTest(uint input,
+            int expected)
+        {
+            var solution191 = new Solution191();
+            var actual = solution191.HammingWeight_Shift(input);
+
+            actual.Should().Be(expected);
+        }
+        
+        [TestCase(0U,
+            0)]
+        [TestCase(11U,
+            3)]
+        [TestCase(128U,
+            1)]
+        [TestCase(4294967290U,
+            30)]
+        [TestCase(4294967293U,
+            31)]
+        [TestCase(4294967295U,
+            32)]
+        public void HammingWeight_DivideTest(uint input,
+            int expected)
+        {
+            var solution191 = new Solution191();
+            var actual = solution191.HammingWeight_Divide(input);
+
+            actual.Should().Be(expected);
+        }
+    }
+}

eb303452bd2b1e20da23331cffdfa1d9ef5bd7cb
RoodyWang <roodywang@boyu66.net>
2021-09-29 17:52:29 +0800

    add problem 12, 13

---
+++ LeetCode/Solution012.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+
+namespace LeetCode
+{
+    public class Solution012
+    {
+        public string IntToRoman(int num)
+        {
+            var sBuilder = new StringBuilder();
+            while (num > 0)
+            {
+                var temp = GetRoman(num);
+                num -= temp.Minus;
+                sBuilder.Append(temp.Result);
+            }
+
+            return sBuilder.ToString();
+        }
+
+        private (string Result, int Minus) GetRoman(int input)
+        {
+            if (input >= 1000)
+            {
+                return ("M", 1000);
+            }
+            else if (input >= 900)
+            {
+                return ("CM", 900);
+            }
+            else if (input >= 500)
+            {
+                return ("D", 500);
+            }
+            else if (input >= 400)
+            {
+                return ("CD", 400);
+            }
+            else if (input >= 100)
+            {
+                return ("C", 100);
+            }
+            else if (input >= 90)
+            {
+                return ("XC", 90);
+            }
+            else if (input >= 50)
+            {
+                return ("L", 50);
+            }
+            else if (input >= 40)
+            {
+                return ("XL", 40);
+            }
+            else if (input >= 10)
+            {
+                return ("X", 10);
+            }
+            else if (input >= 9)
+            {
+                return ("IX", 9);
+            }
+            else if (input >= 5)
+            {
+                return ("V", 5);
+            }
+            else if (input >= 4)
+            {
+                return ("IV", 4);
+            }
+            else
+            {
+                return ("I", 1);
+            }
+        }
+    }
+}
---
+++ LeetCodeTests/Solution013Test.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Solution013Test
+    {
+        [TestCase("III",
+            3)]
+        [TestCase("IV",
+            4)]
+        [TestCase("IX",
+            9)]
+        [TestCase("LVIII",
+            58)]
+        [TestCase("MCMXCIV",
+            1994)]
+        [TestCase("MMMCMXCIX",
+            3999)]
+        [TestCase("MMCM",
+            2900)]
+        public void RomanToIntTest(string input,
+            int expected)
+        {
+            var solution013 = new Solution013();
+
+            var actual = solution013.RomanToInt(input);
+
+            actual.Should().Be(expected);
+        }
+    }
+}

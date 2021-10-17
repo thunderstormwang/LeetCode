6169a68cf4aa3cef15dc81e831abffe60e5f2f16
ThunderstormWang <thunderstormwang@gmail.com>
2021-10-17 21:29:04 +0800

    add problem 169

---
+++ LeetCodeTests/Solution169Test.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Solution169Test
+    {
+        [TestCase(new int[] { 1 },
+            1)]
+        [TestCase(new int[] { 3, 2, 3 },
+            3)]
+        [TestCase(new int[] { 2, 2, 1, 1, 1, 2, 2 },
+            2)]
+        public void MajorityElementTest(int[] inputArray,
+            int expected)
+        {
+            var solution169 = new Solution169();
+            var actual = solution169.MajorityElement(inputArray);
+
+            actual.Should().Be(expected);
+        }
+    }
+}

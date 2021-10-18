fb5bbcd246ea4d1a34be8419221e777d3e95315c
RoodyWang <roodywang@boyu66.net>
2021-10-18 16:49:45 +0800

    modify UnitTest

--- LeetCodeTests/Solution070Test.cs
+++ LeetCodeTests/Solution070Test.cs
@@ -7,8 +7,10 @@ namespace LeetCodeTests
     [TestFixture]
     public class Solution070Test
     {
+        [TestCase(1, 1)]
         [TestCase(2, 2)]
         [TestCase(3, 3)]
+        [TestCase(4, 5)]
         public void ClimbStairsTest(int x, int expected)
         {
             var solution70 = new Solution70();

2b72c0ae0b40d636b111eb8afa7fd8d23fef5a66
ThunderstormWang <thunderstormwang@gmail.com>
2021-10-28 23:52:36 +0800

    add problem 593

---
+++ LeetCode/Solution0593.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class Solution0593
+    {
+        public bool ValidSquare(int[] p1,
+            int[] p2,
+            int[] p3,
+            int[] p4)
+        {
+            var lengthDict = new HashSet<double>();
+
+            lengthDict.Add(LengthPower(p1,
+                p2));
+            lengthDict.Add(LengthPower(p1,
+                p3));
+            lengthDict.Add(LengthPower(p1,
+                p4));
+            
+            lengthDict.Add(LengthPower(p2,
+                p3));
+            lengthDict.Add(LengthPower(p2,
+                p4));
+            
+            lengthDict.Add(LengthPower(p3,
+                p4));
+
+            return lengthDict.Count == 2;
+        }
+
+        private double LengthPower(int[] p1,
+            int[] p2)
+        {
+            return Math.Pow(p1[0] - p2[0],
+                2) + Math.Pow(p1[1] - p2[1],
+                2);
+        }
+    }
+}
---
+++ LeetCodeTests/Solution0593Test.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Solution0593Test
+    {
+        [TestCase(new int[] { 0, 0 },
+            new int[] { 1, 1 },
+            new int[] { 1, 0 },
+            new int[] { 0, 1 },
+            true)]
+        [TestCase(new int[] { 0, 0 },
+            new int[] { 1, 1 },
+            new int[] { 1, 0 },
+            new int[] { 0, 12 },
+            false)]
+        [TestCase(new int[] { 1, 0 },
+            new int[] { -1, 0 },
+            new int[] { 0, 1 },
+            new int[] { 0, -1 },
+            true)]
+        [TestCase(new int[] { 0, 0 },
+            new int[] { 5, 0 },
+            new int[] { 5, 4 },
+            new int[] { 0, 4 },
+            false)]
+        public void ValidSquareTest(int[] p1,
+            int[] p2,
+            int[] p3,
+            int[] p4,
+            bool expected)
+        {
+            var solution = new Solution0593();
+            var actual = solution.ValidSquare(p1,
+                p2,
+                p3,
+                p4);
+
+            actual.Should().Be(expected);
+        }
+    }
+}

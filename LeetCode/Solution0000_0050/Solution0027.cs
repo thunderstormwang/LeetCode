dab01963df38f5c60826a3e5dc3497236bc20365
ThunderstormWang <thunderstormwang@gmail.com>
2021-11-07 22:11:42 +0800

    modify problem 27

---
+++ LeetCode/Solution0000_0050/Solution0027.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Solution0000_0050
+{
+    public class Solution0027
+    {
+        public int RemoveElement(int[] nums,
+            int val)
+        {
+            var slow = 0;
+            for (var fast = 0; fast < nums.Length; fast++)
+            {
+                if (nums[fast] == val)
+                {
+                    continue;
+                }
+
+                (nums[slow], nums[fast]) = (nums[fast], nums[slow]);
+                slow++;
+            }
+
+            return slow;
+        }
+
+        // Time: O(N)
+        // Space: O(1)
+    }
+}

69afda651840e10479ec29ffb2fe1b156017a505
RoodyWang <roodywang@boyu66.net>
2021-11-17 10:03:53 +0800

    modify problem 1824

--- LeetCode/Solution0201_9999/Solution1824.cs
+++ LeetCode/Solution0201_9999/Solution1824.cs
@@ -6,16 +6,16 @@ namespace LeetCode.Solution0201_9999
     {
         public int MinSideJumps(int[] obstacles)
         {
-            var array = new int [obstacles.Length][];
+            var dp = new int [obstacles.Length][];
 
-            for (var i = 0; i < array.Length; i++)
+            for (var i = 0; i < dp.Length; i++)
             {
-                array[i] = new int [3];
+                dp[i] = new int [3];
             }
 
-            array[0][1] = 0;
-            array[0][0] = 1;
-            array[0][2] = 1;
+            dp[0][0] = 1;
+            dp[0][1] = 0;
+            dp[0][2] = 1;
 
             for (var i = 1; i < obstacles.Length; i++)
             {
@@ -23,23 +23,26 @@ namespace LeetCode.Solution0201_9999
                 {
                     if (obstacles[i - 1] == (k + 1) || obstacles[i] == (k + 1))
                     {
-                        array[i][k] = 99;
+                        dp[i][k] = 999999;
                     }
                     else
                     {
                         var aLane = (k + 1) % 3;
                         var bLane = (k + 2) % 3;
-                        array[i][k] = Math.Min(array[i - 1][k],
-                            Math.Min(array[i - 1][aLane],
-                                array[i - 1][bLane]) + 1);
+                        dp[i][k] = Math.Min(dp[i - 1][k],
+                            Math.Min(dp[i - 1][aLane],
+                                dp[i - 1][bLane]) + 1);
                     }
                 }
             }
 
-            return Math.Min(array[obstacles.Length - 1][0],
-                Math.Min(array[obstacles.Length - 1][1],
-                    array[obstacles.Length - 1][2]));
+            return Math.Min(dp[obstacles.Length - 1][0],
+                Math.Min(dp[obstacles.Length - 1][1],
+                    dp[obstacles.Length - 1][2]));
         }
+        
+        // 如果該賽道在 i 點有障礙, 即視為不可到達
+        // 如果該賽道在 i-1 點有障礙, 即視 i 為不可到達
         
         // https://www.youtube.com/watch?v=6-49f0eystc
         // ??

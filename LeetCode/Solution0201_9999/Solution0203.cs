eee3a98d78d7acd74f796eae6ff6fec2406ffb83
ThunderstormWang <thunderstormwang@gmail.com>
2021-11-19 12:01:17 +0800

    add problem 203

---
+++ LeetCode/Solution0201_9999/Solution0203.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Solution0201_9999
+{
+    public class Solution0203
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            var dummyNode = new ListNode(0);
+            dummyNode.next = head;
+            var preNode = dummyNode;
+
+            while (head != null)
+            {
+                if (head.val == val)
+                {
+                    preNode.next = head.next;
+                }
+                else
+                {
+                    preNode = head;
+                }
+
+                head = head.next;
+            }
+
+            return dummyNode.next;
+        }
+        
+        // Time: O(N)
+        // Space: O(1)
+    }
+}

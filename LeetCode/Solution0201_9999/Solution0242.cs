ebdfc090f6f8260416c8ac05e69059f30742e52e
ThunderstormWang <thunderstormwang@gmail.com>
2021-11-21 12:13:38 +0800

    add problem 242

---
+++ LeetCode/Solution0201_9999/Solution0242.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Solution0201_9999
+{
+    public class Solution0242
+    {
+        public bool IsAnagram_Version1(string s,
+            string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            var dict = new Dictionary<char, int>();
+            foreach (var item in s)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict.Add(item,
+                        0);
+                }
+
+                dict[item] += 1;
+            }
+
+            foreach (var item in t)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    return false;
+                }
+
+                dict[item] -= 1;
+                if (dict[item] < 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool IsAnagram_Version2(string s,
+            string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            var record = new int[26];
+
+            foreach (var item in s)
+            {
+                record[item - 'a'] += 1;
+            }
+
+            foreach (var item in t)
+            {
+                record[item - 'a'] -= 1;
+            }
+
+            return record.All(item => item >= 0);
+        }
+
+        // Time: O(N)
+        // Space: O(N)
+    }
+}

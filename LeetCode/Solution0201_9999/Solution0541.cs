bb83a473825cdf347b58d52820d55127ed78f496
RoodyWang <roodywang@boyu66.net>
2021-11-25 14:50:20 +0800

    add problem 541

---
+++ LeetCode/Solution0201_9999/Solution0541.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Solution0201_9999
+{
+    public class Solution0541
+    {
+        public string ReverseStr(string s,
+            int k)
+        {
+            var charArray = s.ToArray();
+            var begin = 0;
+
+            while (begin < s.Length)
+            {
+                var low = begin;
+                var high = Math.Min(begin + k,
+                    s.Length) - 1;
+
+                while (low < high)
+                {
+                    (charArray[low], charArray[high]) = (charArray[high], charArray[low]);
+                    low++;
+                    high--;
+                }
+
+                begin += 2 * k;
+            }
+
+            return new string(charArray);
+        }
+        
+        // Time: O(N)
+        // Space: O(N)
+    }
+}

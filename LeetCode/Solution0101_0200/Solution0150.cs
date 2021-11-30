3e00a72622ef7001445193abdfbcdaa2de029d89
RoodyWang <roodywang@boyu66.net>
2021-11-30 15:08:39 +0800

    add problem 150

---
+++ LeetCode/Solution0101_0200/Solution0150.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Solution0101_0200
+{
+    public class Solution0150
+    {
+        public int EvalRPN(string[] tokens)
+        {
+            var dict = new Dictionary<string, Func<int, int, int>>()
+            {
+                {
+                    "+", (left,
+                        right) => left + right
+                },
+                {
+                    "-", (left,
+                        right) => left - right
+                },
+                {
+                    "*", (left,
+                        right) => left * right
+                },
+                {
+                    "/", (left,
+                        right) => left / right
+                }
+            };
+
+            var stack = new Stack<int>();
+
+            foreach (var item in tokens)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    var right = stack.Pop();
+                    var left = stack.Pop();
+                    stack.Push(dict[item](left,
+                        right));
+                }
+                else
+                {
+                    stack.Push(Convert.ToInt32(item));
+                }
+            }
+
+            return stack.Pop();
+        }
+        
+        // Time: O(N)
+        // Space: O(N)
+    }
+}

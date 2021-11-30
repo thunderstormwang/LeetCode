3eeb6fcd79c676f027f0a81d038e3094c985d1e8
RoodyWang <roodywang@boyu66.net>
2021-11-30 13:59:30 +0800

    add problem 147

---
+++ LeetCode/Solution0201_9999/Solution1047.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Solution0201_9999
+{
+    public class Solution1047
+    {
+        public string RemoveDuplicates(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (var item in s)
+            {
+                if (stack.Count == 0)
+                {
+                    stack.Push(item);
+                    continue;
+                }
+
+                if (stack.Peek() == item)
+                {
+                    stack.Pop();
+                    continue;
+                }
+
+                stack.Push(item);
+            }
+
+            var sBuilder = new StringBuilder();
+            while (stack.Count != 0)
+            {
+                sBuilder.Insert(0, stack.Pop());
+            }
+
+            return sBuilder.ToString();
+        }
+    }
+}
---
+++ LeetCodeTests/SolutionTest0201_9999/Solution1047Test.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0201_9999;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0201_9999
+{
+    [TestFixture]
+    public class Solution1047Test
+    {
+        [TestCase("abbaca",
+            "ca")]
+        [TestCase("azxxzy",
+            "ay")]
+        [TestCase("aababaab",
+            "ba")]
+        [TestCase("abbbaca",
+            "abaca")]
+        [TestCase("aaaaaaaaa",
+            "a")]
+        public void RemoveDuplicates(string s,
+            string expected)
+        {
+            var solution = new Solution1047();
+            var actual = solution.RemoveDuplicates(s);
+
+            actual.Should().Be(expected);
+        }
+    }
+}

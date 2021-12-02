3658cc8dac3adf8ab99752ac0c73793a3f757881
RoodyWang <roodywang@boyu66.net>
2021-12-02 10:19:25 +0800

    add problem 347

---
+++ LeetCode/Solution0201_9999/Solution0347.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Solution0201_9999
+{
+    public class Solution0347
+    {
+        private class Comp : IComparer<KeyValuePair<int, int>>
+        {
+            public int Compare(KeyValuePair<int, int> x,
+                KeyValuePair<int, int> y)
+            {
+                return x.Value == y.Value ? x.Key.CompareTo(y.Key) : x.Value.CompareTo(y.Value);
+            }
+        }
+
+        public int[] TopKFrequentTest(int[] nums,
+            int k)
+        {
+            var dict = new Dictionary<int, int>();
+
+            foreach (var item in nums)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict[item] = 0;
+                }
+
+                dict[item]++;
+            }
+
+            var set = new SortedSet<KeyValuePair<int, int>>(new Comp());
+            foreach (var item in dict)
+            {
+                set.Add(item);
+                if (set.Count > k)
+                {
+                    set.Remove(set.Min);
+                }
+            }
+
+            return set.Select(s => s.Key).ToArray();
+        }
+
+        // 用 Dictionary
+        // Time: O(N) + O(N*log(N)) = O(N*log(N))
+        // Space: O(N)
+    }
+}
---
+++ LeetCodeTests/SolutionTest0201_9999/Solution0347Test.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0201_9999;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0201_9999
+{
+    [TestFixture]
+    public class Solution0347Test
+    {
+        [TestCase(new int[] { 1, 1, 1, 2, 2, 3 },
+            2,
+            new int[] { 1, 2 })]
+        [TestCase(new int[] { 1 },
+            1,
+            new int[] { 1 })]
+        [TestCase(new int[] { 3,0,1,0 },
+            1,
+            new int[] { 0 })]
+        public void TopKFrequent(int[] nums,
+            int k,
+            int[] expected)
+        {
+            var solution = new Solution0347();
+            var actual = solution.TopKFrequentTest(nums,
+                k);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

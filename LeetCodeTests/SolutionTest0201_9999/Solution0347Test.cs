9a6de4ccace094c1e31f634738a2d23321a28a34
ThunderstormWang <thunderstormwang@gmail.com>
2021-12-05 17:47:32 +0800

    modify problem 347

--- LeetCodeTests/SolutionTest0201_9999/Solution0347Test.cs
+++ LeetCodeTests/SolutionTest0201_9999/Solution0347Test.cs
@@ -13,15 +13,47 @@ namespace LeetCodeTests.SolutionTest0201_9999
         [TestCase(new int[] { 1 },
             1,
             new int[] { 1 })]
-        [TestCase(new int[] { 3,0,1,0 },
+        [TestCase(new int[] { 3, 0, 1, 0 },
             1,
             new int[] { 0 })]
-        public void TopKFrequent(int[] nums,
+        [TestCase(new int[] { -1, 1, 4, -4, 3, 5, 4, -2, 3, -1 },
+            3,
+            new int[] { -1, 3, 4 })]
+        [TestCase(new int[] { 4, 1, -1, 2, -1, 2, 3 },
+            2,
+            new int[] { -1, 2 })]
+        public void TopKFrequent_SortedSetTest(int[] nums,
             int k,
             int[] expected)
         {
             var solution = new Solution0347();
-            var actual = solution.TopKFrequentTest(nums,
+            var actual = solution.TopKFrequentTest_SortedSet(nums,
+                k);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestCase(new int[] { 1, 1, 1, 2, 2, 3 },
+            2,
+            new int[] { 1, 2 })]
+        [TestCase(new int[] { 1 },
+            1,
+            new int[] { 1 })]
+        [TestCase(new int[] { 3, 0, 1, 0 },
+            1,
+            new int[] { 0 })]
+        [TestCase(new int[] { -1, 1, 4, -4, 3, 5, 4, -2, 3, -1 },
+            3,
+            new int[] { -1, 3, 4 })]
+        [TestCase(new int[] { 4, 1, -1, 2, -1, 2, 3 },
+            2,
+            new int[] { -1, 2 })]
+        public void TopKFrequent_HeapArrayTest(int[] nums,
+            int k,
+            int[] expected)
+        {
+            var solution = new Solution0347();
+            var actual = solution.TopKFrequentTest_HeapArray(nums,
                 k);
 
             actual.Should().BeEquivalentTo(expected);

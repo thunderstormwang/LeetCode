42423c04ad9703585f8eba1b75b3dc066e737af7
ThunderstormWang <thunderstormwang@gmail.com>
2021-12-06 20:54:43 +0800

    add problem 102

---
+++ LeetCode/Solution0101_0200/Solution0102.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Solution0101_0200
+{
+    public class Solution0102
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            var result = new List<IList<int>>();
+            var queue = new Queue<TreeNode>();
+
+            queue.Enqueue(root);
+            while (queue.Count != 0)
+            {
+                var count = queue.Count;
+                var list = new List<int>();
+                while (count > 0)
+                {
+                    var node = queue.Dequeue();
+                    if (node != null)
+                    {
+                        list.Add(node.val);
+                        queue.Enqueue(node.left);
+                        queue.Enqueue(node.right);
+                    }
+
+                    count--;
+                }
+
+                if (list.Count != 0)
+                {
+                    result.Add(list);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeTests/SolutionTest0101_0200/Solution0102Test.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using LeetCode.Solution0101_0200;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0101_0200
+{
+    [TestFixture]
+    public class Solution0102Test
+    {
+        [TestCaseSource(nameof(TestCases))]
+        public void LevelOrder(int?[] array,
+            IList<IList<int>> expected)
+        {
+            var root = Utility.BuildTree(array,
+                0);
+
+            var solution = new Solution0102();
+            var actual = solution.LevelOrder(root);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        private static readonly object[] TestCases =
+        {
+            new object[]
+            {
+                new int?[] { 3, 9, 20, null, null, 15, 7 },
+                new int[][]
+                {
+                    new int[] { 3 },
+                    new int[] { 9, 20 },
+                    new int[] { 15, 7 }
+                }
+            },
+            new object[]
+            {
+                new int?[] { 1 },
+                new int[][]
+                {
+                    new int[] { 1 }
+                }
+            },
+            new object[]
+            {
+                new int?[] { },
+                new int[][] { }
+            }
+        };
+    }
+}

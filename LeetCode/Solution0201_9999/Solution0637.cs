a0325466d9b5131632f816d209efb6d2f7d21dc7
RoodyWang <roodywang@boyu66.net>
2021-12-07 16:35:14 +0800

    add problem 637

---
+++ LeetCode/Solution0201_9999/Solution0637.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Solution0201_9999
+{
+    public class Solution0637
+    {
+        public IList<double> AverageOfLevels(TreeNode root)
+        {
+            var result = new List<double>();
+            var queue = new Queue<TreeNode>();
+
+            queue.Enqueue(root);
+            while (queue.Count != 0)
+            {
+                var nodeCountInCurrLevel = queue.Count;
+                var levelSum = 0d;
+                for (var i = 0; i < nodeCountInCurrLevel; i++)
+                {
+                    var node = queue.Dequeue();
+
+                    levelSum += node.val;
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+
+                result.Add(levelSum / (double)nodeCountInCurrLevel);
+            }
+
+            return result;
+        }
+    }
+    
+    // Time: O(N)
+    // Space: O(N)
+}

ac9978f69e9a99f5e0436159254a353f3fd26e9b
RoodyWang <roodywang@boyu66.net>
2021-12-10 16:49:14 +0800

    modify problem 670

--- LeetCode/Solution0201_9999/Solution0670.cs
+++ LeetCode/Solution0201_9999/Solution0670.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+using System.Linq;
 
 namespace LeetCode.Solution0201_9999
 {
     public class Solution0670
     {
-        public int MaximumSwap(int num)
+        public int MaximumSwap_Version1(int num)
         {
-            var str = num.ToString();
+            var charArray = num.ToString().ToArray();
             var rightTopIndex = -1;
-            for (var i = str.Length - 1; i > 0; i--)
+            for (var i = charArray.Length - 1; i > 0; i--)
             {
-                if (str[i] - str[i - 1] <= 0)
+                if (charArray[i] - charArray[i - 1] <= 0)
                 {
                     continue;
                 }
@@ -21,7 +22,7 @@ namespace LeetCode.Solution0201_9999
                 }
                 else
                 {
-                    if (str[i] > str[rightTopIndex])
+                    if (charArray[i] > charArray[rightTopIndex])
                     {
                         rightTopIndex = i;
                     }
@@ -33,35 +34,75 @@ namespace LeetCode.Solution0201_9999
                 return num;
             }
 
-            while (rightTopIndex < str.Length - 1 && str[rightTopIndex] == str[rightTopIndex + 1])
+            while (rightTopIndex < charArray.Length - 1 && charArray[rightTopIndex] == charArray[rightTopIndex + 1])
             {
                 rightTopIndex++;
             }
 
             for (var i = 0; i < rightTopIndex; i++)
             {
-                if (str[i] >= str[rightTopIndex])
+                if (charArray[i] >= charArray[rightTopIndex])
                 {
                     continue;
                 }
 
-                var temp = str[i];
-                str = str.Insert(i + 1,
-                    str[rightTopIndex].ToString()).Remove(i,
-                    1);
-                str = str.Insert(rightTopIndex + 1,
-                    temp.ToString()).Remove(rightTopIndex,
-                    1);
+                (charArray[i], charArray[rightTopIndex]) = (charArray[rightTopIndex], charArray[i]);
                 break;
             }
 
-            return Convert.ToInt32(str);
+            return Convert.ToInt32(new string(charArray));
+        }
+
+        public int MaximumSwap_Version2(int num)
+        {
+            var charArray = num.ToString().ToArray();
+            var rightIndex = -1;
+            var leftIndex = -1;
+            var maxIndex = -1;
+
+            for (var i = charArray.Length - 1; i >= 0; i--)
+            {
+                if (maxIndex == -1 || charArray[i] > charArray[maxIndex])
+                {
+                    maxIndex = i;
+                    continue;
+                }
+
+                if (charArray[i] >= charArray[maxIndex])
+                {
+                    continue;
+                }
+
+                leftIndex = i;
+                rightIndex = maxIndex;
+            }
+
+            if (leftIndex == -1)
+            {
+                return num;
+            }
+
+            (charArray[leftIndex], charArray[rightIndex]) = (charArray[rightIndex], charArray[leftIndex]);
+
+
+            return Convert.ToInt32(new string(charArray));
         }
     }
 
+    // Version1 我想的解法
+
     // 從右往左找最高點
     // 如果一個高點都沒找到, 代表數字已從大排到小, 沒有交換的必要
-    // 如果有最高點, 從左邊開始找比最高點小的數字並作交換
+    // 如果有最高點, 如果與最高點相鄰的右邊有同樣的數字, 將最高點重覆右移(被 1993 這案例搞到),
+    // 再從左邊開始找比最高點小的數字並作交換
+
+    // =====
+
+    // Version2 官方解法更有效率... 一個迴圈就解了
+    // 在迴圈內, 由右往右尋找
+    //   如果遇到比當前最大值的更大值出現, 更新最大值的 index
+    //   否前若當前位置的數字比最大值更小, 更新左右的 index
+    // 最後做左右 index 的值做 swap
 
     // Time: O(N)
     // Space: O(1)
--- LeetCodeTests/SolutionTest0201_9999/Solution0670Test.cs
+++ LeetCodeTests/SolutionTest0201_9999/Solution0670Test.cs
@@ -19,11 +19,32 @@ namespace LeetCodeTests.SolutionTest0201_9999
             333)]
         [TestCase(323,
             332)]
-        public void MaximumSwap(int num,
+        public void MaximumSwap_Version1(int num,
             int expected)
         {
             var solution = new Solution0670();
-            var actual = solution.MaximumSwap(num);
+            var actual = solution.MaximumSwap_Version1(num);
+
+            actual.Should().Be(expected);
+        }
+        
+        [TestCase(2736,
+            7236)]
+        [TestCase(9973,
+            9973)]
+        [TestCase(1993,
+            9913)]
+        [TestCase(9678,
+            9876)]
+        [TestCase(333,
+            333)]
+        [TestCase(323,
+            332)]
+        public void MaximumSwap_Version2(int num,
+            int expected)
+        {
+            var solution = new Solution0670();
+            var actual = solution.MaximumSwap_Version2(num);
 
             actual.Should().Be(expected);
         }

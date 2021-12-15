0f6b7d5818f5fde775494f6814dad3af1548b19d
RoodyWang <roodywang@boyu66.net>
2021-12-15 18:20:34 +0800

    add problem 589, 590

---
+++ LeetCode/Solution0201_9999/Solution0590.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using FluentAssertions.Execution;
+
+namespace LeetCode.Solution0201_9999
+{
+    public class Solution0590
+    {
+        public IList<int> Postorder_Recursive(Node root)
+        {
+            var result = new List<int>();
+
+            DepthFirstSearch(root,
+                result);
+
+            return result;
+        }
+
+        public IList<int> Postorder_Iterative(Node root)
+        {
+            var result = new List<int>();
+            var stack = new Stack<Node>();
+
+            stack.Push(root);
+            while (stack.Count != 0)
+            {
+                var node = stack.Pop();
+                if (node == null)
+                {
+                    continue;
+                }
+
+                result.Add(node.val);
+                if (node.children == null)
+                {
+                    continue;
+                }
+
+                foreach (var item in node.children)
+                {
+                    stack.Push(item);
+                }
+            }
+
+            result.Reverse();
+
+            return result;
+        }
+
+        private void DepthFirstSearch(Node root,
+            IList<int> result)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            if (root.children != null)
+            {
+                foreach (var item in root.children)
+                {
+                    DepthFirstSearch(item,
+                        result);
+                }
+            }
+
+            result.Add(root.val);
+        }
+    }
+}

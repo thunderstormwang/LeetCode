e6ad0b34313ea851ac70e5baabb39022e7c54f01
ThunderstormWang <thunderstormwang@gmail.com>
2022-01-14 15:29:29 +0800

    add problem 113

---
+++ LeetCode/Solution0101_0200/Solution0113.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Solution0101_0200
+{
+    public class Solution0113
+    {
+        public IList<IList<int>> PathSum_Recursive(TreeNode root,
+            int targetSum)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            DepthFirstSearch(root,
+                targetSum,
+                new List<int>(),
+                result);
+
+            return result;
+        }
+
+        public IList<IList<int>> PathSum_Iterative(TreeNode root,
+            int targetSum)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            var stack = new Stack<(TreeNode Node, int TargetSum, List<int> CurrPath)>();
+
+            stack.Push((root, targetSum, new List<int>()));
+            while (stack.Count != 0)
+            {
+                var item = stack.Pop();
+                item.TargetSum -= item.Node.val;
+                item.CurrPath.Add(item.Node.val);
+                if (item.Node.left == null && item.Node.right == null && item.TargetSum == 0)
+                {
+                    result.Add(item.CurrPath);
+                    continue;
+                }
+
+                if (item.Node.right != null)
+                {
+                    stack.Push((item.Node.right, item.TargetSum,
+                        new List<int>(item.CurrPath.ToArray())));
+                }
+
+                if (item.Node.left != null)
+                {
+                    stack.Push((item.Node.left, item.TargetSum,
+                        new List<int>(item.CurrPath.ToArray())));
+                }
+            }
+
+            return result;
+        }
+
+        private void DepthFirstSearch(TreeNode root,
+            int targetSum,
+            IList<int> currPath,
+            IList<IList<int>> allPaths)
+        {
+            targetSum -= root.val;
+            currPath.Add(root.val);
+            if (root.left == null && root.right == null && targetSum == 0)
+            {
+                allPaths.Add(currPath.ToArray());
+                currPath.RemoveAt(currPath.Count - 1);
+                return;
+            }
+
+            if (root.left != null)
+            {
+                DepthFirstSearch(root.left,
+                    targetSum,
+                    currPath,
+                    allPaths);
+            }
+
+            if (root.right != null)
+            {
+                DepthFirstSearch(root.right,
+                    targetSum,
+                    currPath,
+                    allPaths);
+            }
+
+            currPath.RemoveAt(currPath.Count - 1);
+        }
+    }
+    
+    // Time: O(N)
+    // Space: O(log(N))
+}
---
+++ LeetCodeTests/SolutionTest0101_0200/Solution0113Test.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using LeetCode.Solution0101_0200;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0101_0200
+{
+    [TestFixture]
+    public class Solution0113Test
+    {
+        [TestCaseSource(nameof(TestCases))]
+        public void PathSum_RecursiveTest(int?[] array,
+            int targetSum,
+            IList<IList<int>> expected)
+        {
+            var root = Utility.BuildTree(array,
+                0);
+
+            var solution = new Solution0113();
+            var actual = solution.PathSum_Recursive(root,
+                targetSum);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [TestCaseSource(nameof(TestCases))]
+        public void PathSum_IterativeTest(int?[] array,
+            int targetSum,
+            IList<IList<int>> expected)
+        {
+            var root = Utility.BuildTree(array,
+                0);
+
+            var solution = new Solution0113();
+            var actual = solution.PathSum_Iterative(root,
+                targetSum);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        private static readonly object[] TestCases =
+        {
+            new object[]
+            {
+                new int?[] { 5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, null, 5, 1 },
+                22,
+                new int[][]
+                {
+                    new int[] { 5, 4, 11, 2 },
+                    new int[] { 5, 8, 4, 5 }
+                }
+            },
+            new object[]
+            {
+                new int?[] { 1, 2, 3 },
+                5,
+                new int[][] { }
+            },
+            new object[]
+            {
+                new int?[] { },
+                0,
+                new int[][] { }
+            },
+            new object[]
+            {
+                new int?[] { 1, 2 },
+                1,
+                new int[][] { }
+            }
+        };
+    }
+}

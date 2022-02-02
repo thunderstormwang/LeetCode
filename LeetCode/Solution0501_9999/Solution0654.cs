4c477337ea5b1df9c67b0d88a3eed7b4d44260b1
ThunderstormWang <thunderstormwang@gmail.com>
2022-02-02 11:43:51 +0800

    add problem 654

---
+++ LeetCode/Solution0501_9999/Solution0654.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode.Solution0501_9999
+{
+    public class Solution0654
+    {
+        public TreeNode ConstructMaximumBinaryTree(int[] nums)
+        {
+            return ConstructMaximumBinaryTree(nums,
+                0,
+                nums.Length - 1);
+        }
+
+        private TreeNode ConstructMaximumBinaryTree(int[] nums,
+            int begin,
+            int end)
+        {
+            var maxIndex = begin;
+            for (var i = begin + 1; i <= end; i++)
+            {
+                if (nums[maxIndex] < nums[i])
+                {
+                    maxIndex = i;
+                }
+            }
+
+            var node = new TreeNode(nums[maxIndex]);
+            if (begin < maxIndex)
+            {
+                node.left = ConstructMaximumBinaryTree(nums,
+                    begin,
+                    maxIndex - 1);
+            }
+
+            if (maxIndex < end)
+            {
+                node.right = ConstructMaximumBinaryTree(nums,
+                    maxIndex + 1,
+                    end);
+            }
+
+            return node;
+        }
+    }
+    
+    // Time: O(N^2)
+    // Space: 最差情況下: O(N), 平均: O(log(N))
+}
---
+++ LeetCodeTests/SolutionTest0501_9999/Solution0654Test.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0501_9999;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0501_9999
+{
+    [TestFixture]
+    public class Solution0654Test
+    {
+        [TestCaseSource(nameof(TestCases))]
+        public void ConstructMaximumBinaryTree(int[] nums,
+            int?[] expectedArray)
+        {
+            var expected = Utility.BuildTree(expectedArray,
+                0);
+
+            var solution = new Solution0654();
+
+            var actual = solution.ConstructMaximumBinaryTree(nums);
+
+            Utility.CheckBinaryTree(actual,
+                expected).Should().BeTrue();
+        }
+
+        private static readonly object[] TestCases =
+        {
+            new object[]
+            {
+                new int[] { 3, 2, 1, 6, 0, 5 },
+                new int?[] { 6, 3, 5, null, 2, 0, null, null, null, null, 1 }
+            },
+            new object[]
+            {
+                new int[] { 3, 2, 1 },
+                new int?[] { 3, null, 2, null, null, null, 1 }
+            }
+        };
+    }
+}

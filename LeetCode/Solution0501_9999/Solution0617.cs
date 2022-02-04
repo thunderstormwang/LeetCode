3c222f24ad96adc36dae54d43da85f055aca9c81
ThunderstormWang <thunderstormwang@gmail.com>
2022-02-04 11:44:02 +0800

    add problem 617

---
+++ LeetCode/Solution0501_9999/Solution0617.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Resources;
+
+namespace LeetCode.Solution0501_9999
+{
+    public class Solution0617
+    {
+        public TreeNode MergeTrees_Recursive(TreeNode root1,
+            TreeNode root2)
+        {
+            if (root1 == null && root2 == null)
+            {
+                return null;
+            }
+
+            var value = 0;
+            value += root1?.val ?? 0;
+            value += root2?.val ?? 0;
+            var result = new TreeNode(value);
+
+            result.left = MergeTrees_Recursive(root1?.left,
+                root2?.left);
+            result.right = MergeTrees_Recursive(root1?.right,
+                root2?.right);
+
+            return result;
+        }
+
+        public TreeNode MergeTrees_Iterative(TreeNode root1,
+            TreeNode root2)
+        {
+            var stack = new Stack<TreeNode>();
+            var parentStack = new Stack<(TreeNode Parent, bool IsLeft)>();
+
+            var dummyNode = new TreeNode(0);
+            parentStack.Push((dummyNode, true));
+            stack.Push(root2);
+            stack.Push(root1);
+
+            while (stack.Count != 0 && parentStack.Count != 0)
+            {
+                var node1 = stack.Pop();
+                var node2 = stack.Pop();
+                var parentItem = parentStack.Pop();
+
+                if (node1 == null && node2 == null)
+                {
+                    continue;
+                }
+
+                var value = 0;
+                value += node1?.val ?? 0;
+                value += node2?.val ?? 0;
+                var mergeNode = new TreeNode(value);
+
+                if (parentItem.IsLeft)
+                {
+                    parentItem.Parent.left = mergeNode;
+                }
+                else
+                {
+                    parentItem.Parent.right = mergeNode;
+                }
+
+                parentStack.Push((mergeNode, false));
+                stack.Push(node2?.right);
+                stack.Push(node1?.right);
+                parentStack.Push((mergeNode, true));
+                stack.Push(node2?.left);
+                stack.Push(node1?.left);
+            }
+
+            return dummyNode.left;
+        }
+    }
+
+    // Time: O(N)
+    // Space: O(N)
+}
---
+++ LeetCodeTests/SolutionTest0501_9999/Solution0617Test.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0501_9999;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0501_9999
+{
+    [TestFixture]
+    public class Solution0617Test
+    {
+        [TestCaseSource(nameof(TestCases))]
+        public void MergeTrees_RecursiveTest(int?[] array1,
+            int?[] array2,
+            int?[] expectedArray)
+        {
+            var root1 = Utility.BuildTree(array1,
+                0);
+            var root2 = Utility.BuildTree(array2,
+                0);
+            var expected = Utility.BuildTree(expectedArray,
+                0);
+
+            var solution = new Solution0617();
+
+            var actual = solution.MergeTrees_Recursive(root1,
+                root2);
+
+            Utility.CheckBinaryTree(actual,
+                expected).Should().BeTrue();
+        }
+        
+        [TestCaseSource(nameof(TestCases))]
+        public void MergeTrees_IterativeTest(int?[] array1,
+            int?[] array2,
+            int?[] expectedArray)
+        {
+            var root1 = Utility.BuildTree(array1,
+                0);
+            var root2 = Utility.BuildTree(array2,
+                0);
+            var expected = Utility.BuildTree(expectedArray,
+                0);
+
+            var solution = new Solution0617();
+
+            var actual = solution.MergeTrees_Iterative(root1,
+                root2);
+
+            Utility.CheckBinaryTree(actual,
+                expected).Should().BeTrue();
+        }
+
+        private static readonly object[] TestCases =
+        {
+            new object[]
+            {
+                new int?[] { 1, 3, 2, 5 },
+                new int?[] { 2, 1, 3, null, 4, null, 7 },
+                new int?[] { 3, 4, 5, 5, 4, null, 7 }
+            },
+            new object[]
+            {
+                new int?[] { 1 },
+                new int?[] { 1, 2 },
+                new int?[] { 2, 2 }
+            },
+            new object[]
+            {
+                new int?[] { 1 },
+                new int?[] { },
+                new int?[] { 1 }
+            },
+            new object[]
+            {
+                new int?[] { },
+                new int?[] { },
+                new int?[] { }
+            }
+        };
+    }
+}

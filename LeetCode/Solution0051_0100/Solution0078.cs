266b34c3438f12d1d7f57dce6de286628eea75aa
ThunderstormWang <thunderstormwang@gmail.com>
2022-12-03 15:22:06 +0800

    modify problem 78

---
+++ LeetCode/Solution0051_0100/Solution0078.cs
@@ -0,0 +1,55 @@
+﻿namespace LeetCode.Solution0051_0100;
+
+public class Solution0078
+{
+    private int[] _nums;
+
+    public IList<IList<int>> Subsets_WithEndCondition(int[] nums)
+    {
+        _nums = nums;
+
+        var result = new List<IList<int>>();
+        Backtrack_WithEndCondition(0, new List<int>(), result);
+        return result;
+    }
+
+    public IList<IList<int>> Subsets_NoEndCondtion(int[] nums)
+    {
+        _nums = nums;
+        
+        var result = new List<IList<int>>();
+        Backtrack__NoEndCondition(0, new List<int>(), result);
+
+        return result;
+    }
+
+    private void Backtrack_WithEndCondition(int index, List<int> curr, IList<IList<int>> result)
+    {
+        if (index == _nums.Length)
+        {
+            result.Add(curr.ToArray());
+            return;
+        }
+
+        Backtrack_WithEndCondition(index + 1, curr, result);
+
+        curr.Add(_nums[index]);
+        Backtrack_WithEndCondition(index + 1, curr, result);
+        curr.RemoveAt(curr.Count - 1);
+    }
+
+    private void Backtrack__NoEndCondition(int index, IList<int> curr, IList<IList<int>> result)
+    {
+        result.Add(curr.ToArray());
+
+        for (var i = index; i < _nums.Length; i++)
+        {
+            curr.Add(_nums[i]);
+            Backtrack__NoEndCondition(i + 1, curr, result);
+            curr.Remove(_nums[i]);
+        }
+    }
+    
+    // Time: O(2^N) -- 每個元素可選或不選
+    // Time: O(N) -- 最多遞迴 N 層
+}
---
+++ LeetCodeTests/SolutionTest0051_0100/Solution0078Test.cs
@@ -0,0 +1,78 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0051_0100;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0051_0100;
+
+[TestFixture]
+public class Solution0078Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void SubsetsTest_WithEndCondition(int[] inputArray, int[][] expectedArray)
+    {
+        var solution = new Solution0078();
+        var actual = solution.Subsets_WithEndCondition(inputArray);
+
+        var expected = new List<List<int>>();
+        foreach (var item in expectedArray)
+        {
+            expected.Add(new List<int>(item));
+        }
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+    
+    [TestCaseSource(nameof(TestCases))]
+    public void SubsetsTest_NoEndCondition(int[] inputArray, int[][] expectedArray)
+    {
+        var solution = new Solution0078();
+        var actual = solution.Subsets_NoEndCondtion(inputArray);
+
+        var expected = new List<List<int>>();
+        foreach (var item in expectedArray)
+        {
+            expected.Add(new List<int>(item));
+        }
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new int[] { 0 },
+            new int[][]
+            {
+                new int[] { },
+                new int[] { 0 }
+            }
+        },
+        new object[]
+        {
+            new int[] { 0, 1 },
+            new int[][]
+            {
+                new int[] { },
+                new int[] { 0 },
+                new int[] { 1 },
+                new int[] { 0, 1 }
+            }
+        },
+        new object[]
+        {
+            new int[] { 0, 1, 2 },
+            new int[][]
+            {
+                new int[] { },
+                new int[] { 0 },
+                new int[] { 1 },
+                new int[] { 2 },
+                new int[] { 0, 1 },
+                new int[] { 0, 2 },
+                new int[] { 1, 2 },
+                new int[] { 0, 1, 2 }
+            }
+        }
+    };
+}

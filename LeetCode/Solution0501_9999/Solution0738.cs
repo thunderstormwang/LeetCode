c32655933a5ea4570aa9c5c730a60df1a06e6abb
Roody_Wang <roody_wang@pxmart.com.tw>
2023-01-13 16:00:22 +0800

    add problem 738

---
+++ LeetCode/Solution0501_9999/Solution0738.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace LeetCode.Solution0501_9999;
+
+public class Solution0738
+{
+    public int MonotoneIncreasingDigits(int n)
+    {
+        var strBuilder = new StringBuilder(n.ToString());
+        var firstNineIndex = strBuilder.Length;
+        for (var i = strBuilder.Length - 1; i > 0; i--)
+        {
+            if (strBuilder[i - 1] > strBuilder[i])
+            {
+                firstNineIndex = i;
+                strBuilder[i] = '9';
+                strBuilder[i - 1]--;
+            }
+        }
+
+        for (var i = firstNineIndex; i < strBuilder.Length; i++)
+        {
+            strBuilder[i] = '9';
+        }
+
+        return Int32.Parse(strBuilder.ToString());
+    }
+}
---
+++ LeetCodeTests/SolutionTest0501_9999/Solution0738Test.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0501_9999;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0501_9999;
+
+[TestFixture]
+public class Solution0738Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void PartitionLabelsTest(int n, int expected)
+    {
+        var solution = new Solution0738();
+        var actual = solution.MonotoneIncreasingDigits(n);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            10,
+            9
+        },
+        new object[]
+        {
+            1234,
+            1234
+        },
+        new object[]
+        {
+            332,
+            299
+        },
+        new object[]
+        {
+            100,
+            99
+        }
+    };
+}

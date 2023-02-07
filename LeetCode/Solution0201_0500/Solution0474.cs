f0a57dd46afe5c17daf578b8da30a334ee1cf387
ThunderstormWang <thunderstormwang@gmail.com>
2023-02-07 22:57:06 +0800

    add problem 474

---
+++ LeetCode/Solution0201_0500/Solution0474.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCode.Solution0201_0500;
+
+public class Solution0474
+{
+    public int FindMaxForm(string[] strs, int m, int n)
+    {
+        var dp = new int [m + 1][];
+        for (var i = 0; i < m + 1; i++)
+        {
+            dp[i] = new int[n + 1];
+        }
+
+        foreach (var item in strs)
+        {
+            var zero = 0;
+            var one = 0;
+            for (var i = 0; i < item.Length; i++)
+            {
+                if (item[i] == '0')
+                {
+                    zero++;
+                }
+                else
+                {
+                    one++;
+                }
+            }
+
+            for (var i = m; i >= zero; i--)
+            {
+                for (var j = n; j >= one; j--)
+                {
+                    dp[i][j] = Math.Max(dp[i][j], dp[i - zero][j - one] + 1);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
---
+++ LeetCodeTests/SolutionTest0201_0500/Solution0474Test.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0201_0500;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0201_0500;
+
+[TestFixture]
+public class Solution0474Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void FindMaxForm(string[] strs, int m, int n, int expected)
+    {
+        var solution = new Solution0474();
+        var actual = solution.FindMaxForm(strs, m, n);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new string[] { "10", "0001", "111001", "1", "0" },
+            5,
+            3,
+            4
+        },
+        new object[]
+        {
+            new string[] { "10", "0", "1" },
+            1,
+            1,
+            2
+        }
+    };
+}

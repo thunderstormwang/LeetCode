c041a7f10768e9cd2cbd3b272d0139a249f09193
Roody_Wang <roody_wang@pxmart.com.tw>
2023-02-08 18:07:50 +0800

    add problem 518 未完

---
+++ LeetCode/Solution0501_9999/Solution0518.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace LeetCode.Solution0501_9999;
+
+public class Solution0518
+{
+    public int Change(int amount, int[] coins)
+    {
+        var dp = new int[coins.Length + 1][];
+        for (var i = 0; i < coins.Length + 1; i++)
+        {
+            dp[i] = new int[amount + 1];
+        }
+
+        for (var i = 0; i < coins.Length + 1; i++)
+        {
+            dp[i][0] = 1;
+        }
+
+        for (var i = 0; i < coins.Length; i++)
+        {
+            for (var j = coins[i]; j <= amount; j++)
+            {
+                Debug.WriteLine($"{coins[i]}, dp[{i+1}][{j}] = dp[{i}][{j}] + dp[{i + 1}][{j - coins[i]}] = {dp[i][j] + dp[i + 1][j - coins[i]]}");
+                dp[i + 1][j] = dp[i][j] + dp[i + 1][j - coins[i]];
+            }
+        }
+
+        return dp[coins.Length][amount];
+    }
+}
---
+++ LeetCodeTests/SolutionTest0501_9999/Solution0518Test.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0501_9999;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0501_9999;
+
+[TestFixture]
+public class Solution0518Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void ChangeTest(int amount, int[] coins, int expected)
+    {
+        var solution = new Solution0518();
+        var actual = solution.Change(amount, coins);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            5,
+            new int[] { 1, 2, 5 },
+            4
+        },
+        new object[]
+        {
+            3,
+            new int[] { 2 },
+            0
+        },
+        new object[]
+        {
+            10,
+            new int[] { 10 },
+            1
+        }
+    };
+}

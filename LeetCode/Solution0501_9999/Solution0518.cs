4377f54b8c341080d7b6ae42b9d71d603f85e463
Roody_Wang <roody_wang@pxmart.com.tw>
2023-02-09 09:36:24 +0800

    modify problem 518

--- LeetCode/Solution0501_9999/Solution0518.cs
+++ LeetCode/Solution0501_9999/Solution0518.cs
@@ -4,6 +4,12 @@ namespace LeetCode.Solution0501_9999;
 
 public class Solution0518
 {
+    /// <summary>
+    /// 完全背包 二維陣列
+    /// </summary>
+    /// <param name="amount"></param>
+    /// <param name="coins"></param>
+    /// <returns></returns>
     public int Change_Ver1(int amount, int[] coins)
     {
         var dp = new int[coins.Length + 1][];
@@ -35,15 +41,21 @@ public class Solution0518
         return dp[coins.Length][amount];
     }
 
+    /// <summary>
+    /// 完全背包 一維陣列
+    /// </summary>
+    /// <param name="amount"></param>
+    /// <param name="coins"></param>
+    /// <returns></returns>
     public object Change_Ver2(int amount, int[] coins)
     {
         var dp = new int[amount + 1];
 
         dp[0] = 1;
 
-        for (var j = 0; j <= amount; j++)
+        for (var i = 0; i < coins.Length; i++)
         {
-            for (var i = 0; i < coins.Length; i++)
+            for (var j = 0; j <= amount; j++)
             {
                 if (j - coins[i] >= 0)
                 {

7c30ff741d40c32e36678e25cefe28b99d61e38b
Roody_Wang <roody_wang@pxmart.com.tw>
2023-02-10 17:35:14 +0800

    add problem 139

---
+++ LeetCodeTests/SolutionTest0101_0200/Solution0139Test.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0101_0200;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0101_0200;
+
+[TestFixture]
+public class Solution0139Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void Partition_Index(string s, IList<string> wordDict, bool expected)
+    {
+        var solution = new Solution0139();
+        var actual = solution.WordBreak(s, wordDict);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            "leetcode",
+            new List<string>
+            {
+                "leet", "code"
+            },
+            true
+        },
+        new object[]
+        {
+            "applepenapple",
+            new List<string>
+            {
+                "apple","pen"
+            },
+            true
+        },
+        new object[]
+        {
+            "catsandog",
+            new List<string>
+            {
+                "cats","dog","sand","and","cat"
+            },
+            false
+        }
+    };
+}

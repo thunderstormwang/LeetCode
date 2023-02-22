e9b8c38be71cd1e8e31b9abf3ba3ea557264c0c4
ThunderstormWang <thunderstormwang@gmail.com>
2023-02-22 20:48:04 +0800

    add problem 188

---
+++ LeetCodeTests/SolutionTest0101_0200/Solution0188Test.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0101_0200;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0101_0200;
+
+[TestFixture]
+public class Solution0188Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void MaxProfit(int k, int[] prices, int expected)
+    {
+        var solution = new Solution0188();
+
+        var actual = solution.MaxProfit(k, prices);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            2,
+            new int[] { 2, 4, 1 },
+            2
+        },
+        new object[]
+        {
+            2,
+            new int[] { 3, 2, 6, 5, 0, 3 },
+            7
+        },
+    };
+}
---
+++ Notes/Solution0188.md
@@ -0,0 +1,11 @@
+# Solution0188
+
+## 動態規劃
+
+跟 [Solution123](Solution0123.md) 類似
+
+### 複雜度
+
+#### Time: O(n)
+
+#### Memory: O(k*n)

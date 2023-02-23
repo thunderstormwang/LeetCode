388c0a25a2850048451fca1967627bf94fa97843
ThunderstormWang <thunderstormwang@gmail.com>
2023-02-23 22:40:27 +0800

    add problem 309

---
+++ LeetCode/Solution0201_0500/Solution0309.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Solution0201_0500;
+
+public class Solution0309
+{
+    public int MaxProfit(int[] prices)
+    {
+        if (prices.Length == 1)
+        {
+            return 0;
+        }
+
+        var dp = new int[prices.Length][];
+        for (var i = 0; i < prices.Length; i++)
+        {
+            dp[i] = new int[2];
+        }
+
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        dp[1][0] = Math.Max(dp[0][0], -prices[1]);
+        dp[1][1] = Math.Max(dp[0][1], dp[0][0] + prices[1]);
+
+        for (var i = 2; i < prices.Length; i++)
+        {
+            dp[i][0] = Math.Max(dp[i - 1][0], dp[i - 2][1] - prices[i]);
+            dp[i][1] = Math.Max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+
+        return dp[prices.Length - 1][1];
+    }
+}
---
+++ LeetCodeTests/SolutionTest0201_0500/Solution0309Test.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0201_0500;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0201_0500;
+
+[TestFixture]
+public class Solution0309Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void NumSquares_Ver1Test(int[] prices, int expected)
+    {
+        var solution = new Solution0309();
+        var actual = solution.MaxProfit(prices);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new int[] { 1, 2, 3, 0, 2 },
+            3
+        },
+        new object[]
+        {
+            new int[] { 1 },
+            0
+        },
+        new object[]
+        {
+            new int[] { 1, 2, 4 },
+            3
+        }
+    };
+}

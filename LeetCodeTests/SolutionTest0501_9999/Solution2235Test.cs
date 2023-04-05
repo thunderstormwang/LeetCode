1b7c61a5ec3997c6532027435399a392d484ab37
ThunderstormWang <thunderstormwang@gmail.com>
2023-04-05 11:22:36 +0800

    add problem 2235

---
+++ LeetCodeTests/SolutionTest0501_9999/Solution2235Test.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0501_9999;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0501_9999;
+
+[TestFixture]
+public class Solution2235Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void SumTest(int num1, int num2, int expected)
+    {
+        var solution = new Solution2235();
+        var actual = solution.Sum(num1, num2);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            12,
+            5,
+            17
+        },
+        new object[]
+        {
+            -10,
+            4,
+            -6
+        }
+    };
+}

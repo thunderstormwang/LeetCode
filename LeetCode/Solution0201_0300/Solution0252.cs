4f52271679e121eed43c3f79c8245cb8ebfb742b
ThunderstormWang <thunderstormwang@gmail.com>
2023-04-10 21:11:12 +0800

    add problem 252

---
+++ LeetCode/Solution0201_0300/Solution0252.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Solution0201_0300;
+
+public class Solution0252
+{
+    public bool CanAttendMeetings(int[][] intervals)
+    {
+        Array.Sort(intervals, (x, y) =>
+        {
+            if (x[0] < y[0])
+            {
+                return -1;
+            }
+
+            if (x[0] > y[0])
+            {
+                return 1;
+            }
+
+            if (x[1] < y[1])
+            {
+                return -1;
+            }
+
+            return 1;
+        });
+
+        for (var i = 1; i < intervals.Length; i++)
+        {
+            if (intervals[i][0] < intervals[i - 1][1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ LeetCodeTests/SolutionTest0201_0300/Solution0252Test.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0201_0300;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0301_0500;
+
+[TestFixture]
+public class Solution0252Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void OOOTest(int[][] nums, bool expected)
+    {
+        var solution = new Solution0252();
+        var actual = solution.CanAttendMeetings(nums);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new int[][]
+            {
+                new int[] { 0, 30 },
+                new int[] { 5, 10 },
+                new int[] { 15, 20 },
+            },
+            false
+        },
+        new object[]
+        {
+            new int[][]
+            {
+                new int[] { 7,10 },
+                new int[] { 2,4 }
+            },
+            true
+        }
+    };
+}

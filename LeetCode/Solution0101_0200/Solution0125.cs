d034be3c37578253ef927db41ccb019091777ad0
ThunderstormWang <thunderstormwang@gmail.com>
2023-04-22 10:38:15 +0800

    modify problem 125

---
+++ LeetCode/Solution0101_0200/Solution0125.cs
@@ -0,0 +1,72 @@
+﻿namespace LeetCode.Solution0101_0200;
+
+public class Solution0125
+{
+    public bool IsPalindrome_Ver1(string s)
+    {
+        var left = 0;
+        var right = s.Length - 1;
+
+        while (left < right)
+        {
+            while ((s[left] < 'a' || s[left] > 'z') 
+                   && (s[left] < 'A' || s[left] > 'Z') 
+                   && (s[left] < '0' || s[left] > '9') 
+                   && left < right)
+            {
+                left++;
+            }
+
+            while ((s[right] < 'a' || s[right] > 'z') 
+                   && (s[right] < 'A' || s[right] > 'Z') 
+                   && (s[right] < '0' || s[right] > '9') 
+                   && right > left)
+            {
+                right--;
+            }
+
+            if (Char.ToLower(s[left]) != Char.ToLower(s[right]))
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public bool IsPalindrome_Ver2(string s)
+    {
+        s = s.ToLower();
+
+        var left = 0;
+        var right = s.Length - 1;
+
+        while (right > left)
+        {
+            if (!char.IsLetterOrDigit(s[left]))
+            {
+                left++;
+                continue;
+            }
+
+            if (!char.IsLetterOrDigit(s[right]))
+            {
+                right--;
+                continue;
+            }
+
+            if (s[left] != s[right])
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
---
+++ Notes/Solution0101_0200/Solution0125.md
@@ -0,0 +1,15 @@
+# Solution0125
+
+## Two Pointer
+
+雙指針，left 從最左邊開始，right 從最右邊開始  
+跑迴圈
+- 指針在遇到非英數字就跳過  
+- 如果 s[left] != [right]，即回傳 false
+迴圈跑完就回傳 true
+
+### 複雜度
+
+#### Time: O(n)
+
+#### Memory: O(1)

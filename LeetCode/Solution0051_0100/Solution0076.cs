c32a1585487f89d11b1511d557d4e9d79df83222
ThunderstormWang <thunderstormwang@gmail.com>
2023-04-23 17:35:50 +0800

    add problem 76

---
+++ LeetCode/Solution0051_0100/Solution0076.cs
@@ -0,0 +1,115 @@
+﻿namespace LeetCode.Solution0051_0100;
+
+public class Solution0076
+{
+    public string MinWindow_Ver1(string s, string t)
+    {
+        var bestLeft = -1;
+        var minLength = int.MaxValue;
+        var notMatched = 0;
+        var dict = new Dictionary<char, int>();
+
+        for (var i = 0; i < t.Length; i++)
+        {
+            if (!dict.ContainsKey(t[i]))
+            {
+                dict.Add(t[i], 1);
+                notMatched++;
+            }
+            else
+            {
+                dict[t[i]]++;
+            }
+        }
+
+        var left = 0;
+        var right = 0;
+
+        while (right < s.Length)
+        {
+            if (dict.ContainsKey(s[right]))
+            {
+                dict[s[right]]--;
+                if (dict[s[right]] == 0)
+                {
+                    notMatched--;
+                }
+            }
+
+            while (notMatched == 0)
+            {
+                if (minLength > right - left + 1)
+                {
+                    bestLeft = left;
+                    minLength = right - left + 1;
+                }
+
+                if (dict.ContainsKey(s[left]))
+                {
+                    dict[s[left]]++;
+                    if (dict[s[left]] > 0)
+                    {
+                        notMatched++;
+                    }
+                }
+
+                left++;
+            }
+
+            right++;
+        }
+
+        return bestLeft == -1 ? string.Empty : s.Substring(bestLeft, minLength);
+    }
+
+    public string MinWindow_Ver2(string s, string t)
+    {
+        var minLength = int.MaxValue;
+        var bestLeft = -1;
+        var array = new int [128];
+        var notMatched = 0;
+
+        for (var i = 0; i < t.Length; i++)
+        {
+            if (array[t[i]] == 0)
+            {
+                notMatched++;
+            }
+
+            array[t[i]]++;
+        }
+
+        var left = 0;
+        var right = 0;
+
+        while (right < s.Length)
+        {
+            array[s[right]]--;
+            if (array[s[right]] == 0)
+            {
+                notMatched--;
+            }
+
+            while (notMatched == 0)
+            {
+                array[s[left]]++;
+                if (array[s[left]] > 0)
+                {
+                    notMatched++;
+                }
+
+                if (minLength > right - left + 1)
+                {
+                    minLength = right - left + 1;
+                    bestLeft = left;
+                }
+
+                left++;
+            }
+
+            right++;
+        }
+
+        return bestLeft == -1 ? string.Empty : s.Substring(bestLeft, minLength);
+    }
+}
---
+++ LeetCodeTests/SolutionTest0051_0100/Solution0076Test.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0051_0100;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0051_0100;
+
+[TestFixture]
+[Category("SolutionTest0051_0100")]
+public class Solution0076Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void MinDistance_Ver1Test(string s, string t, string expected)
+    {
+        var solution = new Solution0076();
+        var actual = solution.MinWindow_Ver1(s, t);
+
+        actual.Should().Be(expected);
+    }
+    
+    [TestCaseSource(nameof(TestCases))]
+    public void MinDistance_Ver2Test(string s, string t, string expected)
+    {
+        var solution = new Solution0076();
+        var actual = solution.MinWindow_Ver2(s, t);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            "ADOBECODEBANC",
+            "ABC",
+            "BANC"
+        },
+        new object[]
+        {
+            "a",
+            "a",
+            "a"
+        },
+        new object[]
+        {
+            "a",
+            "aa",
+            ""
+        }
+    };
+}

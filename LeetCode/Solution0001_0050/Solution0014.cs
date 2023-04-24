562d7742c58966a595d639d6646520c2c9235b4d
ThunderstormWang <thunderstormwang@gmail.com>
2023-04-24 21:59:36 +0800

    modify problem 14

--- LeetCode/Solution0001_0050/Solution0014.cs
+++ LeetCode/Solution0001_0050/Solution0014.cs
@@ -1,36 +1,32 @@
 ﻿using System.Text;
 
-namespace LeetCode.Solution0001_0050
+namespace LeetCode.Solution0001_0050;
+
+public class Solution0014
 {
-    public class Solution0014
+    public string LongestCommonPrefix(string[] strs)
     {
-        public string LongestCommonPrefix(string[] strs)
+        if (strs.Length == 1)
         {
-            if (strs.Length == 1)
-            {
-                return strs[0];
-            }
-
-            var result = new StringBuilder();
-
-            for (var i = 0; i < strs[0].Length; i++)
-            {
-                var temp = strs[0][i];
-                for (var j = 1; j < strs.Length; j++)
-                {
-                    if ((strs[j].Length - 1) < i || temp != strs[j][i])
-                    {
-                        return result.ToString();
-                    }
-                }
-
-                result.Append(temp);
-            }
-
-            return result.ToString();
+            return strs[0];
         }
-        
-        // Time: O(N), N = 所有字串的長度和
-        // Space: O(1)
+
+        var result = new StringBuilder();
+
+        for (var i = 0; i < strs[0].Length; i++)
+        {
+            var temp = strs[0][i];
+            for (var j = 1; j < strs.Length; j++)
+            {
+                if (strs[j].Length - 1 < i || temp != strs[j][i])
+                {
+                    return result.ToString();
+                }
+            }
+
+            result.Append(temp);
+        }
+
+        return result.ToString();
     }
 }
--- LeetCodeTests/SolutionTest0001_0050/Solution0014Test.cs
+++ LeetCodeTests/SolutionTest0001_0050/Solution0014Test.cs
@@ -2,49 +2,48 @@
 using LeetCode.Solution0001_0050;
 using NUnit.Framework;
 
-namespace LeetCodeTests.SolutionTest0001_0050
+namespace LeetCodeTests.SolutionTest0001_0050;
+
+[TestFixture]
+[Category("SolutionTest0001_0050")]
+public class Solution0014Test
 {
-    [TestFixture]
-    [Category("SolutionTest0001_0050")]
-    public class Solution0014Test
+    [TestCaseSource(nameof(TestCases))]
+    public void LongestCommonPrefixTest(string[] strs, string expected)
     {
-        [TestCaseSource(nameof(TestCases))]
-        public void LongestCommonPrefixTest(string[] strs, string expected)
-        {
-            var solution = new Solution0014();
+        var solution = new Solution0014();
 
-            var actual = solution.LongestCommonPrefix(strs);
+        var actual = solution.LongestCommonPrefix(strs);
 
-            actual.Should().Be(expected);
-        }
-
-        private static readonly object[] TestCases =
-        {
-            new object[]
-            {
-                new string[] { "flower", "flow", "flight" },
-                "fl"
-            },
-            new object[]
-            {
-                new string[] { "flower", "flower", "flower" },
-                "flower"
-            },
-            new object[]
-            {
-                new string[] { "dog", "racecar", "car" },
-                string.Empty
-            },
-            new object[]
-            {
-                new string[] { "dog" },
-                "dog"
-            },
-            new object[]
-            {
-                new string[] { "dog", string.Empty, "car" },
-                string.Empty
-            }
-        };
+        actual.Should().Be(expected);
     }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new string[] { "flower", "flow", "flight" },
+            "fl"
+        },
+        new object[]
+        {
+            new string[] { "flower", "flower", "flower" },
+            "flower"
+        },
+        new object[]
+        {
+            new string[] { "dog", "racecar", "car" },
+            string.Empty
+        },
+        new object[]
+        {
+            new string[] { "dog" },
+            "dog"
+        },
+        new object[]
+        {
+            new string[] { "dog", string.Empty, "car" },
+            string.Empty
+        }
+    };
 }

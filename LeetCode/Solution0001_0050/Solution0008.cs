dc94d4f76ff8e2680eb666775e4c0c159c2ca47a
ThunderstormWang <thunderstormwang@gmail.com>
2023-04-29 20:29:42 +0800

    add problem 8

---
+++ LeetCode/Solution0001_0050/Solution0008.cs
@@ -0,0 +1,96 @@
+﻿namespace LeetCode.Solution0001_0050;
+
+public class Solution0008
+{
+    public int MyAtoi(string s)
+    {
+        var digitStart = 0;
+        var digitStr = string.Empty;
+        var negative = false;
+        var foundSign = false;
+        var i = 0;
+
+        if (string.IsNullOrEmpty(s))
+        {
+            return 0;
+        }
+
+        while (i < s.Length && (s[i] < '0' || s[i] > '9') && !foundSign)
+        {
+            if (s[i] != ' ' && s[i] != '+' && s[i] != '-')
+            {
+                return 0;
+            }
+
+            switch (s[i])
+            {
+                case ' ':
+                    break;
+                case '+':
+                case '-':
+                {
+                    if (foundSign)
+                    {
+                        return 0;
+                    }
+
+                    negative = s[i] == '-';
+                    foundSign = true;
+                    break;
+                }
+            }
+
+            i++;
+        }
+
+        if (i >= s.Length || s[i] < '0' || s[i] > '9')
+        {
+            return 0;
+        }
+
+        digitStart = i;
+        while (digitStart < s.Length && s[digitStart] == '0')
+        {
+            digitStart++;
+        }
+
+        i = digitStart;
+        while (i < s.Length && s[i] >= '0' && s[i] <= '9')
+        {
+            i++;
+        }
+
+        digitStr = s.Substring(digitStart, i - digitStart);
+
+        if (negative && "2147483648".Length < digitStr.Length)
+        {
+            return int.MinValue;
+        }
+
+        if (!negative && "2147483647".Length < digitStr.Length)
+        {
+            return int.MaxValue;
+        }
+
+        if (negative && "2147483648".Length == digitStr.Length && string.Compare("2147483648", digitStr) == -1)
+        {
+            return int.MinValue;
+        }
+
+        if (!negative && "2147483647".Length == digitStr.Length && string.Compare("2147483647", digitStr) == -1)
+        {
+            return int.MaxValue;
+        }
+
+        i = 0;
+        var result = 0;
+
+        while (i < digitStr.Length)
+        {
+            result = result * 10 + digitStr[i] - '0';
+            i++;
+        }
+
+        return negative ? -result : result;
+    }
+}
--- LeetCodeTests/SolutionTest0001_0050/Solution0008Test.cs
+++ LeetCodeTests/SolutionTest0001_0050/Solution0008Test.cs
@@ -1,6 +1,29 @@
-﻿namespace LeetCodeTests.SolutionTest0001_0050;
+﻿using FluentAssertions;
+using LeetCode.Solution0001_0050;
+using NUnit.Framework;
 
+namespace LeetCodeTests.SolutionTest0001_0050;
+
+[TestFixture]
+[Category("SolutionTest0001_0050")]
 public class Solution0008Test
 {
-    
+    [TestCase("42", 42)]
+    [TestCase("   -42", -42)]
+    [TestCase("4193 with words", 4193)]
+    [TestCase("words and 987", 0)]
+    [TestCase("-91283472332", -2147483648)]
+    [TestCase("+-12", 0)]
+    [TestCase("2147483646", 2147483646)]
+    [TestCase("21474836460", 2147483647)]
+    [TestCase("  +  413", 0)]
+    [TestCase("", 0)]
+    [TestCase("+", 0)]
+    public void MyAtoiTest(string s, int expected)
+    {
+        var solution = new Solution0008();
+        var actual = solution.MyAtoi(s);
+
+        actual.Should().Be(expected);
+    }
 }

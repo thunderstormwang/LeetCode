8b8e4fb7ff1d9fde71efbb0c8c0f4219306dde24
ThunderstormWang <thunderstormwang@gmail.com>
2023-05-01 14:38:32 +0800

    add problem 36

--- LeetCode/Solution0001_0050/Solution0036.cs
+++ LeetCode/Solution0001_0050/Solution0036.cs
@@ -1,63 +1,131 @@
-﻿using System;
-using System.Collections.Generic;
+﻿namespace LeetCode.Solution0001_0050;
 
-namespace LeetCode.Solution0001_0050
+public class Solution0036
 {
-    public class Solution0036
+    public bool IsValidSudoku_Ver1(char[][] board)
     {
-        public bool IsValidSudoku(char[][] board)
+        for (var i = 0; i < board.Length; i++)
         {
+            var used = new HashSet<char>();
+            for (var j = 0; j < board[i].Length; j++)
+            {
+                if (board[i][j] != '.' && !used.Add(board[i][j]))
+                {
+                    return false;
+                }
+            }
+        }
+
+        for (var j = 0; j < board[0].Length; j++)
+        {
+            var used = new HashSet<char>();
             for (var i = 0; i < board.Length; i++)
             {
-                var used = new HashSet<char>();
-                for (var j = 0; j < board[i].Length; j++)
+                if (board[i][j] != '.' && !used.Add(board[i][j]))
                 {
-                    if (board[i][j] != '.' && !used.Add(board[i][j]))
-                    {
-                        return false;
-                    }
+                    return false;
                 }
             }
-
-            Console.WriteLine("Row");
-
-            for (var j = 0; j < board[0].Length; j++)
-            {
-                var used = new HashSet<char>();
-                for (var i = 0; i < board.Length; i++)
-                {
-                    if (board[i][j] != '.' && !used.Add(board[i][j]))
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            Console.WriteLine("Column");
-
-            var xList = new List<int>() { 0, 0, 0, 1, 1, 1, -1, -1, -1 };
-            var yList = new List<int>() { 0, 1, -1, 0, 1, -1, 0, 1, -1 };
-
-            for (var i = 1; i < board.Length; i += 3)
-            {
-                for (var j = 1; j < board[i].Length; j += 3)
-                {
-                    var used = new HashSet<char>();
-                    for (int k = 0; k < xList.Count; k++)
-                    {
-                        var target = board[i + xList[k]][j + yList[k]];
-                        if (target != '.' && !used.Add(target))
-                        {
-                            return false;
-                        }
-                    }
-                }
-            }
-
-            return true;
         }
-        
-        // Time: O(1)
-        // Space: O(1)
+
+        var xList = new List<int>() { 0, 0, 0, 1, 1, 1, -1, -1, -1 };
+        var yList = new List<int>() { 0, 1, -1, 0, 1, -1, 0, 1, -1 };
+
+        for (var i = 1; i < board.Length; i += 3)
+        {
+            for (var j = 1; j < board[i].Length; j += 3)
+            {
+                var used = new HashSet<char>();
+                for (int k = 0; k < xList.Count; k++)
+                {
+                    var target = board[i + xList[k]][j + yList[k]];
+                    if (target != '.' && !used.Add(target))
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public object IsValidSudoku_Ver2(char[][] board)
+    {
+        var shifts = new int[9][]
+        {
+            new int[] { -1, -1 },
+            new int[] { -1, 0 },
+            new int[] { -1, 1 },
+            new int[] { 0, -1 },
+            new int[] { 0, 0 },
+            new int[] { 0, 1 },
+            new int[] { 1, -1 },
+            new int[] { 1, 0 },
+            new int[] { 1, 1 }
+        };
+
+        for (var i = 0; i < board.Length; i++)
+        {
+            var array = new int [10];
+            for (var j = 0; j < board[i].Length; j++)
+            {
+                if (board[i][j] == '.')
+                {
+                    continue;
+                }
+
+                if (array[board[i][j] - '0'] > 0)
+                {
+                    return false;
+                }
+
+                array[board[i][j] - '0']++;
+            }
+        }
+
+        for (var j = 0; j < board[0].Length; j++)
+        {
+            var array = new int [10];
+            for (var i = 0; i < board.Length; i++)
+            {
+                if (board[i][j] == '.')
+                {
+                    continue;
+                }
+
+                if (array[board[i][j] - '0'] > 0)
+                {
+                    return false;
+                }
+
+                array[board[i][j] - '0']++;
+            }
+        }
+
+        for (var i = 1; i < board.Length; i += 3)
+        {
+            for (var j = 1; j < board[0].Length; j += 3)
+            {
+                var array = new int [10];
+                for (var k = 0; k < shifts.Length; k++)
+                {
+                    var target = board[i + shifts[k][0]][j + shifts[k][1]];
+                    if (target == '.')
+                    {
+                        continue;
+                    }
+
+                    if (array[target - '0'] > 0)
+                    {
+                        return false;
+                    }
+
+                    array[target - '0']++;
+                }
+            }
+        }
+
+        return true;
     }
 }
--- LeetCodeTests/SolutionTest0001_0050/Solution0036Test.cs
+++ LeetCodeTests/SolutionTest0001_0050/Solution0036Test.cs
@@ -2,72 +2,79 @@
 using LeetCode.Solution0001_0050;
 using NUnit.Framework;
 
-namespace LeetCodeTests.SolutionTest0001_0050
+namespace LeetCodeTests.SolutionTest0001_0050;
+
+[TestFixture]
+[Category("SolutionTest0001_0050")]
+public class Solution0036Test
 {
-    [TestFixture]
-    [Category("SolutionTest0001_0050")]
-    public class Solution0036Test
+    [TestCaseSource(nameof(TestCases))]
+    public void IsValidSudoku_Ver1Test(char[][] board, bool expected)
     {
-        [TestCaseSource(nameof(TestCases))]
-        public void IsValidSudoku(char[][] board, bool expected)
-        {
-            var solution = new Solution0036();
+        var solution = new Solution0036();
+        var actual = solution.IsValidSudoku_Ver1(board);
 
-            var actual = solution.IsValidSudoku(board);
-
-            actual.Should().Be(expected);
-        }
-
-        private static readonly object[] TestCases =
-        {
-            new object[]
-            {
-                new char[][]
-                {
-                    new char[] { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
-                    new char[] { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
-                    new char[] { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
-                    new char[] { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
-                    new char[] { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
-                    new char[] { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
-                    new char[] { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
-                    new char[] { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
-                    new char[] { '.', '.', '.', '.', '8', '.', '.', '7', '9' },
-                },
-                true
-            },
-            new object[]
-            {
-                new char[][]
-                {
-                    new char[] { '8', '3', '.', '.', '7', '.', '.', '.', '.' },
-                    new char[] { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
-                    new char[] { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
-                    new char[] { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
-                    new char[] { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
-                    new char[] { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
-                    new char[] { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
-                    new char[] { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
-                    new char[] { '.', '.', '.', '.', '8', '.', '.', '7', '9' },
-                },
-                false
-            },
-            new object[]
-            {
-                new char[][]
-                {
-                    new char[] { '.', '.', '4', '.', '.', '.', '6', '3', '.' },
-                    new char[] { '.', '.', '.', '.', '.', '.', '.', '.', '.' },
-                    new char[] { '5', '.', '.', '.', '.', '.', '.', '9', '.' },
-                    new char[] { '.', '.', '.', '5', '6', '.', '.', '.', '.' },
-                    new char[] { '4', '.', '3', '.', '.', '.', '.', '.', '1' },
-                    new char[] { '.', '.', '.', '7', '.', '.', '.', '.', '.' },
-                    new char[] { '.', '.', '.', '5', '.', '.', '.', '.', '.' },
-                    new char[] { '.', '.', '.', '.', '.', '.', '.', '.', '.' },
-                    new char[] { '.', '.', '.', '.', '.', '.', '.', '.', '.' },
-                },
-                false
-            }
-        };
+        actual.Should().Be(expected);
     }
+    
+    [TestCaseSource(nameof(TestCases))]
+    public void IsValidSudoku_Ver2Test(char[][] board, bool expected)
+    {
+        var solution = new Solution0036();
+        var actual = solution.IsValidSudoku_Ver2(board);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new char[][]
+            {
+                new char[] { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+                new char[] { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+                new char[] { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+                new char[] { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+                new char[] { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+                new char[] { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+                new char[] { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+                new char[] { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+                new char[] { '.', '.', '.', '.', '8', '.', '.', '7', '9' },
+            },
+            true
+        },
+        new object[]
+        {
+            new char[][]
+            {
+                new char[] { '8', '3', '.', '.', '7', '.', '.', '.', '.' },
+                new char[] { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+                new char[] { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+                new char[] { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+                new char[] { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+                new char[] { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+                new char[] { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+                new char[] { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+                new char[] { '.', '.', '.', '.', '8', '.', '.', '7', '9' },
+            },
+            false
+        },
+        new object[]
+        {
+            new char[][]
+            {
+                new char[] { '.', '.', '4', '.', '.', '.', '6', '3', '.' },
+                new char[] { '.', '.', '.', '.', '.', '.', '.', '.', '.' },
+                new char[] { '5', '.', '.', '.', '.', '.', '.', '9', '.' },
+                new char[] { '.', '.', '.', '5', '6', '.', '.', '.', '.' },
+                new char[] { '4', '.', '3', '.', '.', '.', '.', '.', '1' },
+                new char[] { '.', '.', '.', '7', '.', '.', '.', '.', '.' },
+                new char[] { '.', '.', '.', '5', '.', '.', '.', '.', '.' },
+                new char[] { '.', '.', '.', '.', '.', '.', '.', '.', '.' },
+                new char[] { '.', '.', '.', '.', '.', '.', '.', '.', '.' },
+            },
+            false
+        }
+    };
 }

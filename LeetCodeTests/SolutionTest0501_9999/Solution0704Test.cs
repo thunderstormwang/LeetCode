f3fc769d0dbc95523a4db6c2b354d960ea70b56f
ThunderstormWang <thunderstormwang@gmail.com>
2023-05-05 22:05:53 +0800

    modify problem 704

--- LeetCodeTests/SolutionTest0501_9999/Solution0704Test.cs
+++ LeetCodeTests/SolutionTest0501_9999/Solution0704Test.cs
@@ -2,34 +2,55 @@
 using LeetCode.Solution0501_9999;
 using NUnit.Framework;
 
-namespace LeetCodeTests.SolutionTest0501_9999
+namespace LeetCodeTests.SolutionTest0501_9999;
+
+[TestFixture]
+[Category("SolutionTest0501_9999")]
+public class Solution0704Test
 {
-    [TestFixture]
-    [Category("SolutionTest0501_9999")]
-    public class Solution0704Test
+    [TestCaseSource(nameof(TestCases))]
+    public void Search_Recursive(int[] nums, int target, int expected)
     {
-        [TestCase(new int[] { -1, 0, 3, 5, 9, 12 }, 9, 4)]
-        [TestCase(new int[] { -1, 0, 3, 5, 9, 12 }, 2, -1)]
-        [TestCase(new int[] { 5 }, 5, 0)]
-        [TestCase(new int[] { 5 }, -5, -1)]
-        public void Search_Recursive(int[] nums, int target, int expected)
-        {
-            var solution = new Solution0704();
-            var actual = solution.Search_Recursive(nums, target);
+        var solution = new Solution0704();
+        var actual = solution.Search_Recursive(nums, target);
 
-            actual.Should().Be(expected);
-        }
-        
-        [TestCase(new int[] { -1, 0, 3, 5, 9, 12 }, 9, 4)]
-        [TestCase(new int[] { -1, 0, 3, 5, 9, 12 }, 2, -1)]
-        [TestCase(new int[] { 5 }, 5, 0)]
-        [TestCase(new int[] { 5 }, -5, -1)]
-        public void Search_Iterative(int[] nums, int target, int expected)
-        {
-            var solution = new Solution0704();
-            var actual = solution.Search_Iterative(nums, target);
-
-            actual.Should().Be(expected);
-        }
+        actual.Should().Be(expected);
     }
+
+    [TestCaseSource(nameof(TestCases))]
+    public void Search_Iterative(int[] nums, int target, int expected)
+    {
+        var solution = new Solution0704();
+        var actual = solution.Search_Iterative(nums, target);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new int[] { -1, 0, 3, 5, 9, 12 },
+            9,
+            4
+        },
+        new object[]
+        {
+            new int[] { -1, 0, 3, 5, 9, 12 },
+            2,
+            -1
+        },
+        new object[]
+        {
+            new int[] { 5 },
+            5,
+            0
+        },
+        new object[]
+        {
+            new int[] { 5 },
+            -5,
+            -1
+        }
+    };
 }
---
+++ Notes/Solution0501_9999/Solution0704.md
@@ -0,0 +1,19 @@
+# Solution0704
+
+## Iteratvie
+
+### 複雜度
+
+#### Time: O(log(n))
+
+#### Space: O(1)
+
+---
+
+## Recursive
+
+### 複雜度
+
+#### Time: O(log(n))
+
+#### Space: O(1)

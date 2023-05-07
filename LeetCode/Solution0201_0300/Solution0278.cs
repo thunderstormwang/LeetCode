045b4b2525569874b720770882e3e5405a56edbe
ThunderstormWang <thunderstormwang@gmail.com>
2023-05-07 22:37:20 +0800

    add problem 278

---
+++ LeetCode/Solution0201_0300/Solution0278.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode.Solution0201_0300;
+
+public class Solution0278 : VersionControl
+{
+    /// <inheritdoc />
+    public Solution0278(int firstBadVersion) : base(firstBadVersion)
+    {
+    }
+
+    public int FirstBadVersion(int n)
+    {
+        var left = 0;
+        var right = n;
+
+        while (left <= right)
+        {
+            var index = left + (right - left) / 2;
+
+            if (IsBadVersion(index))
+            {
+                right = index - 1;
+            }
+            else
+            {
+                left = index + 1;
+            }
+        }
+
+        return left;
+    }
+}
+
+public class VersionControl
+{
+    private int _firstBadVersion;
+
+    public VersionControl(int firstBadVersion)
+    {
+        _firstBadVersion = firstBadVersion;
+    }
+
+    public bool IsBadVersion(int version)
+    {
+        return version >= _firstBadVersion;
+    }
+}
---
+++ LeetCodeTests/SolutionTest0201_0300/Solution0278Test.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0201_0300;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0201_0300;
+
+[TestFixture]
+[Category("SolutionTest0201_0300")]
+public class Solution0278Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void FirstBadVersionTest(int n, int firstBadVersion, int expected)
+    {
+        var solution = new Solution0278(firstBadVersion);
+        var actual = solution.FirstBadVersion(n);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            5,
+            4,
+            4
+        },
+        new object[]
+        {
+            1,
+            1,
+            1
+        },
+        new object[]
+        {
+            5,
+            5,
+            5
+        },
+        new object[]
+        {
+            5,
+            3,
+            3
+        },
+        new object[]
+        {
+            5,
+            2,
+            2
+        },
+        new object[]
+        {
+            5,
+            1,
+            1
+        }
+    };
+}

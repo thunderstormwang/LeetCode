0b0c0a4d1a6a501d865a7ed54f3a48a9f418e826
ThunderstormWang <thunderstormwang@gmail.com>
2023-05-26 00:13:02 +0800

    add problem 207

---
+++ LeetCode/Solution0201_0300/Solution0207.cs
@@ -0,0 +1,109 @@
+﻿namespace LeetCode.Solution0201_0300;
+
+public class Solution0207
+{
+    public bool CanFinish(int numCourses, int[][] prerequisites)
+    {
+        var adjacentList = new List<List<int>>();
+
+        for (var i = 0; i < numCourses; i++)
+        {
+            adjacentList.Add(new List<int>());
+        }
+
+        for (var i = 0; i < prerequisites.Length; i++)
+        {
+            adjacentList[prerequisites[i][1]].Add(prerequisites[i][0]);
+        }
+
+        var visit = new int [numCourses];
+
+        for (var i = 0; i < numCourses; i++)
+        {
+            if (Cycle(i, visit, adjacentList))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool Cycle(int node, int[] visit, List<List<int>> adjacentList)
+    {
+        if (visit[node] == 1)
+        {
+            return true;
+        }
+
+        if (visit[node] == 2)
+        {
+            return false;
+        }
+
+        visit[node] = 1;
+
+        for (var i = 0; i < adjacentList[node].Count; i++)
+        {
+            if (Cycle(adjacentList[node][i], visit, adjacentList))
+            {
+                return true;
+            }
+        }
+
+        visit[node] = 2;
+
+        return false;
+    }
+
+    public bool CanFinish_Ver2(int numCourses, int[][] prerequisites)
+    {
+        var adjacentList = new List<List<int>>();
+
+        for (var i = 0; i < numCourses; i++)
+        {
+            adjacentList.Add(new List<int>());
+        }
+
+        for (var i = 0; i < prerequisites.Length; i++)
+        {
+            adjacentList[prerequisites[i][1]].Add(prerequisites[i][0]);
+        }
+
+        for (var i = 0; i < numCourses; i++)
+        {
+            var visit = new bool [numCourses];
+            if (DFS(i, i, visit, adjacentList))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool DFS(int start, int curr, bool[] visit, List<List<int>> adjacentList)
+    {
+        if (start == curr && visit[curr])
+        {
+            return true;
+        }
+
+        if (visit[curr])
+        {
+            return false;
+        }
+
+        visit[curr] = true;
+
+        for (var i = 0; i < adjacentList[curr].Count; i++)
+        {
+            if (DFS(start, adjacentList[curr][i], visit, adjacentList))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ LeetCodeTests/SolutionTest0201_0300/Solution0207Test.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0201_0300;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0201_0300;
+
+[TestFixture]
+[Category("SolutionTest0201_0300")]
+public class Solution0207Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void CanFinishTest(int numCourses, int[][] prerequisites, bool expected)
+    {
+        var solution = new Solution0207();
+        var actual = solution.CanFinish(numCourses, prerequisites);
+
+        actual.Should().Be(expected);
+    }
+    
+    [TestCaseSource(nameof(TestCases))]
+    public void CanFinish_Ver2Test(int numCourses, int[][] prerequisites, bool expected)
+    {
+        var solution = new Solution0207();
+        var actual = solution.CanFinish_Ver2(numCourses, prerequisites);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            2,
+            new int[][]
+            {
+                new int[] { 1, 0 }
+            },
+            true
+        },
+        new object[]
+        {
+            2,
+            new int[][]
+            {
+                new int[] { 1, 0 },
+                new int[] { 0, 1 }
+            },
+            false
+        },
+        new object[]
+        {
+            8,
+            new int[][]
+            {
+                new int[] { 1, 0 },
+                new int[] { 2, 6 },
+                new int[] { 1, 7 },
+                new int[] { 5, 1 },
+                new int[] { 6, 4 },
+                new int[] { 7, 0 },
+                new int[] { 0, 5 },
+            },
+            false
+        },
+        new object[]
+        {
+            8,
+            new int[][]
+            {
+                new int[] { 1, 0 },
+                new int[] { 2, 6 },
+                new int[] { 1, 7 },
+                new int[] { 6, 4 },
+                new int[] { 7, 0 },
+                new int[] { 0, 5 },
+            },
+            true
+        }
+    };
+}

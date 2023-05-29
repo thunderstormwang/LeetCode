fa342563e946e2e34e30cca828a006fd00b8fd01
ThunderstormWang <thunderstormwang@gmail.com>
2023-05-29 23:22:59 +0800

    modify problem 994

---
+++ LeetCode/Solution0501_9999/Solution0994.cs
@@ -0,0 +1,64 @@
+﻿namespace LeetCode.Solution0501_9999;
+
+public class Solution0994
+{
+    public int OrangesRotting(int[][] grid)
+    {
+        var directions = new int[][]
+        {
+            new int[] { -1, 0 },
+            new int[] { 1, 0 },
+            new int[] { 0, -1 },
+            new int[] { 0, 1 }
+        };
+
+        var queue = new Queue<int[]>();
+        var freshOrange = 0;
+        var minute = 0;
+
+        for (var i = 0; i < grid.Length; i++)
+        {
+            for (var j = 0; j < grid[i].Length; j++)
+            {
+                if (grid[i][j] == 2)
+                {
+                    queue.Enqueue(new int[] { i, j });
+                }
+
+                if (grid[i][j] == 1)
+                {
+                    freshOrange++;
+                }
+            }
+        }
+
+        while (queue.Count != 0 && freshOrange > 0)
+        {
+            var count = queue.Count;
+
+            for (var cnt = 0; cnt < count; cnt++)
+            {
+                var rotten = queue.Dequeue();
+
+                for (var i = 0; i < directions.Length; i++)
+                {
+                    var row = rotten[0] + directions[i][0];
+                    var column = rotten[1] + directions[i][1];
+
+                    if (row < 0 || row >= grid.Length || column < 0 || column >= grid[row].Length || grid[row][column] != 1)
+                    {
+                        continue;
+                    }
+                    
+                    grid[row][column] = 2;
+                    freshOrange--;
+                    queue.Enqueue(new int[] { row, column });
+                }
+            }
+
+            minute++;
+        }
+
+        return freshOrange == 0 ? minute : -1;
+    }
+}
---
+++ LeetCodeTests/SolutionTest0501_9999/Solution0994Test.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0501_9999;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0501_9999;
+
+[TestFixture]
+[Category("SolutionTest0501_9999")]
+public class Solution0994Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void SortedSquares(int[][] grid, int expected)
+    {
+        var solution = new Solution0994();
+        var actual = solution.OrangesRotting(grid);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new int[][]
+            {
+                new int[] { 2, 1, 1 },
+                new int[] { 1, 1, 0 },
+                new int[] { 0, 1, 1 },
+            },
+            4
+        },
+        new object[]
+        {
+            new int[][]
+            {
+                new int[] { 2, 1, 1 },
+                new int[] { 0, 1, 1 },
+                new int[] { 1, 0, 1 },
+            },
+            -1
+        },
+        new object[]
+        {
+            new int[][]
+            {
+                new int[] { 0, 2 }
+            },
+            0
+        }
+    };
+}

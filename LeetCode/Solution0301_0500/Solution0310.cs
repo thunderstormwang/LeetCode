2dd92453127de4a93867d3e8e721ed9ab0cade32
ThunderstormWang <thunderstormwang@gmail.com>
2023-06-06 23:41:43 +0800

    add problem 310

---
+++ LeetCode/Solution0301_0500/Solution0310.cs
@@ -0,0 +1,154 @@
+﻿namespace LeetCode.Solution0301_0500;
+
+public class Solution0310
+{
+    public IList<int> FindMinHeightTrees_Ver1(int n, int[][] edges)
+    {
+        var result = new List<int>();
+        var minHeight = int.MaxValue;
+        var adjacencyList = new Dictionary<int, List<int>>();
+
+        if (n == 1)
+        {
+            return new List<int>() { 0 };
+        }
+
+        for (var i = 0; i < edges.Length; i++)
+        {
+            if (!adjacencyList.ContainsKey(edges[i][0]))
+            {
+                adjacencyList.Add(edges[i][0], new List<int>());
+            }
+
+            if (!adjacencyList.ContainsKey(edges[i][1]))
+            {
+                adjacencyList.Add(edges[i][1], new List<int>());
+            }
+
+            adjacencyList[edges[i][0]].Add(edges[i][1]);
+            adjacencyList[edges[i][1]].Add(edges[i][0]);
+        }
+
+        for (var i = 0; i < n; i++)
+        {
+            var queue = new Queue<int>();
+            var visited = new int [n];
+            var depth = 0;
+            var hasBreak = false;
+
+            queue.Enqueue(i);
+            visited[i] = 1;
+
+            while (queue.Count != 0)
+            {
+                if (depth > minHeight)
+                {
+                    hasBreak = true;
+                    break;
+                }
+
+                var count = queue.Count;
+                var newElement = false;
+
+                for (var cnt = 0; cnt < count; cnt++)
+                {
+                    var node = queue.Dequeue();
+
+                    for (var j = 0; j < adjacencyList[node].Count; j++)
+                    {
+                        if (visited[adjacencyList[node][j]] == 1)
+                        {
+                            continue;
+                        }
+
+                        if (!newElement)
+                        {
+                            newElement = true;
+                            depth++;
+                        }
+
+                        queue.Enqueue(adjacencyList[node][j]);
+                        visited[adjacencyList[node][j]] = 1;
+                    }
+                }
+            }
+
+            if (hasBreak)
+            {
+                continue;
+            }
+
+            if (depth == minHeight)
+            {
+                result.Add(i);
+            }
+
+            if (depth < minHeight)
+            {
+                result = new List<int>() { i };
+                minHeight = depth;
+            }
+        }
+
+        return result;
+    }
+
+    public IList<int> FindMinHeightTrees_Ver2(int n, int[][] edges)
+    {
+        var adjacencyList = new Dictionary<int, List<int>>();
+        var queue = new Queue<int>();
+
+        if (n == 1)
+        {
+            return new List<int>() { 0 };
+        }
+
+        for (var i = 0; i < edges.Length; i++)
+        {
+            if (!adjacencyList.ContainsKey(edges[i][0]))
+            {
+                adjacencyList.Add(edges[i][0], new List<int>());
+            }
+
+            if (!adjacencyList.ContainsKey(edges[i][1]))
+            {
+                adjacencyList.Add(edges[i][1], new List<int>());
+            }
+
+            adjacencyList[edges[i][0]].Add(edges[i][1]);
+            adjacencyList[edges[i][1]].Add(edges[i][0]);
+        }
+
+        foreach (var item in adjacencyList)
+        {
+            if (item.Value.Count == 1)
+            {
+                queue.Enqueue(item.Key);
+            }
+        }
+
+        var nodeCount = n;
+        while (nodeCount > 2)
+        {
+            var queueCount = queue.Count;
+            nodeCount -= queueCount;
+            for (var cnt = 0; cnt < queueCount; cnt++)
+            {
+                var node = queue.Dequeue();
+
+                for (var i = 0; i < adjacencyList[node].Count; i++)
+                {
+                    var adjNode = adjacencyList[node][i];
+                    adjacencyList[adjNode].Remove(node);
+                    if (adjacencyList[adjNode].Count == 1)
+                    {
+                        queue.Enqueue(adjNode);
+                    }
+                }
+            }
+            
+        }
+
+        return new List<int>(queue.ToArray());
+    }
+}
---
+++ LeetCodeTests/SolutionTest0301_0500/Solution0310Test.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0301_0500;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0301_0500;
+
+[TestFixture]
+[Category("SolutionTest0301_0500")]
+public class Solution0310Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void NumSquares_Ver1Test(int n, int[][] edges, List<int> expected)
+    {
+        var solution = new Solution0310();
+        var actual = solution.FindMinHeightTrees_Ver1(n, edges);
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+    
+    [TestCaseSource(nameof(TestCases))]
+    public void NumSquares_Ver2Test(int n, int[][] edges, List<int> expected)
+    {
+        var solution = new Solution0310();
+        var actual = solution.FindMinHeightTrees_Ver2(n, edges);
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            4,
+            new int[][]
+            {
+                new int[] { 1, 0 },
+                new int[] { 1, 2 },
+                new int[] { 1, 3 }
+            },
+            new List<int> { 1 }
+        },
+        new object[]
+        {
+            6,
+            new int[][]
+            {
+                new int[] { 3, 0 },
+                new int[] { 3, 1 },
+                new int[] { 3, 2 },
+                new int[] { 3, 4 },
+                new int[] { 5, 4 },
+            },
+            new List<int> { 3, 4 }
+        },
+        new object[]
+        {
+            1,
+            new int[][] { },
+            new List<int> { 0 }
+        }
+    };
+}

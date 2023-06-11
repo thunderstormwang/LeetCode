9748c9d0959a5d4973024731436de9116b7474c2
ThunderstormWang <thunderstormwang@gmail.com>
2023-06-11 21:06:24 +0800

    add problem 1730

---
+++ LeetCodeTests/SolutionTest0501_9999/Solution1730Test.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0501_9999;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0501_9999;
+
+[TestFixture]
+[Category("SolutionTest0501_9999")]
+public class Solution1730Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void GetFoodTest(char[][] grid, int expected)
+    {
+        var solution = new Solution1730();
+        var actual = solution.GetFood(grid);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new char[][]
+            {
+                new char[] { 'X', 'X', 'X', 'X', 'X', 'X' },
+                new char[] { 'X', '*', 'O', 'O', 'O', 'X' },
+                new char[] { 'X', 'O', 'O', '#', 'O', 'X' },
+                new char[] { 'X', 'X', 'X', 'X', 'X', 'X' },
+            },
+            3
+        },
+        new object[]
+        {
+            new char[][]
+            {
+                new char[] { 'X', 'X', 'X', 'X', 'X' },
+                new char[] { 'X', '*', 'X', 'O', 'X' },
+                new char[] { 'X', 'O', 'X', '#', 'X' },
+                new char[] { 'X', 'X', 'X', 'X', 'X', },
+            },
+            -1
+        },
+        new object[]
+        {
+            new char[][]
+            {
+                new char[] { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
+                new char[] { 'X', '*', 'O', 'X', 'O', '#', 'O', 'X' },
+                new char[] { 'X', 'O', 'O', 'X', 'O', 'O', 'X', 'X' },
+                new char[] { 'X', 'O', 'O', 'O', 'O', '#', 'O', 'X' },
+                new char[] { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' }
+            },
+            6
+        },
+        new object[]
+        {
+            new char[][]
+            {
+                new char[] { 'O', '*' },
+                new char[] { '#', 'O' }
+            },
+            2
+        },
+        new object[]
+        {
+            new char[][]
+            {
+                new char[] { 'X', '*' },
+                new char[] { '#', 'X' },
+            },
+            -1
+        }
+    };
+}
---
+++ Notes/Solution0501_9999/Solution1730.md
@@ -0,0 +1,27 @@
+# Solution1730
+
+難得 graph 中有一看就會的題目
+
+## Breadth First Search
+
+因為要求找最短路徑的值，用 BFS  
+找到起始點，放入 queue
+
+從 queue 取出元素  
+如果四週有還未起出邊界且還沒探訪過的 cell
+- 如果該 cell 是食物，回傳深度加 1
+- 如果該 cell 是空格，將該 cell 標記為已探訪過，並放入 queue
+清空完一層的 queue，深度加 1
+
+重覆上述步驟直到 queue 為空
+
+### 複雜度
+- grid.length: m
+- grid[0].length: n
+
+#### Time: O(m * n)
+- 尋找起始點: O(m * n)
+- 每個 cell 最多放入 queue 一次 m * n: O(m * n)
+
+#### Space: O(m * n)
+- queue 最多放 m * n: O(m * n)

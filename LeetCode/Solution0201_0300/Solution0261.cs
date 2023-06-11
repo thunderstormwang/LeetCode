cbed597349f04efa746f64a929e3f4d4b70fdb8f
ThunderstormWang <thunderstormwang@gmail.com>
2023-06-11 21:19:02 +0800

    add problem 261 未完成

---
+++ LeetCode/Solution0201_0300/Solution0261.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode.Solution0201_0300;
+
+public class Solution0261
+{
+    public bool ValidTree(int n, int[][] edges)
+    {
+        var queue = new Queue<int>();
+        var inDegree = new int [n];
+        var adjacentList = new Dictionary<int, List<int>>();
+        var nodeCount = 0;
+        var visited = new int [n];
+
+        for(var i = 0; i < n; i++)
+        {
+            adjacentList.Add(i, new List<int>());
+        }
+
+        for(var i = 0; i < edges.Length; i++)
+        {
+            adjacentList[edges[i][0]].Add(edges[i][1]);
+            adjacentList[edges[i][1]].Add(edges[i][0]);
+            inDegree[edges[i][0]]++;
+            inDegree[edges[i][1]]++;
+        }
+        
+        for()
+
+        while(queue.Count != 0)
+        {
+            var i = queue.Dequeue();
+            nodeCount++;
+
+            for(var j = 0; j < adjacentList[i].Count; j++)
+            {
+                inDegree[adjacentList[i][j]]--;
+                adjacentList[adjacentList[i][j]].Remove(i);
+                if(inDegree[adjacentList[i][j]] == 0)
+                {
+                    queue.Enqueue(adjacentList[i][j]);
+                }
+            }
+        }
+        
+        return nodeCount == n;
+    }
+}
---
+++ LeetCodeTests/SolutionTest0201_0300/Solution0261Test.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0201_0300;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0201_0300;
+
+[TestFixture]
+[Category("SolutionTest0201_0300")]
+public class Solution0261Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void CanAttendMeetingsTest(int n, int[][] edges, bool expected)
+    {
+        var solution = new Solution0261();
+        var actual = solution.ValidTree(n, edges);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            5,
+            new int[][]
+            {
+                new int[] { 0, 1 },
+                new int[] { 0, 2 },
+                new int[] { 0, 3 },
+                new int[] { 1, 4 },
+            },
+            true
+        },
+        new object[]
+        {
+            5,
+            new int[][]
+            {
+                new int[] { 0, 1 },
+                new int[] { 1, 2 },
+                new int[] { 2, 3 },
+                new int[] { 1, 3 },
+                new int[] { 1, 4 },
+            },
+            false
+        }
+    };
+}

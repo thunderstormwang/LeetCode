20470420a42b786fd3ae8af3fb0b80cf40a1b562
ThunderstormWang <thunderstormwang@gmail.com>
2023-06-25 12:23:50 +0800

    modify problem 110

--- LeetCode/Solution0101_0200/Solution0110.cs
+++ LeetCode/Solution0101_0200/Solution0110.cs
@@ -1,49 +1,58 @@
 ﻿using LeetCode.Models;
 
-namespace LeetCode.Solution0101_0200
+namespace LeetCode.Solution0101_0200;
+
+public class Solution0110
 {
-    public class Solution0110
+    /// <summary>
+    /// DFS 計算高度
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public bool IsBalanced_Ver1(TreeNode root)
     {
-        public bool IsBalanced_BottomUp(TreeNode root)
+        return DepthFirstSearch_Ver1(root, out _);
+    }
+
+    /// <summary>
+    /// DFS 計算深度
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public bool IsBalanced_Ver2(TreeNode root)
+    {
+        return DepthFirstSearch_Ver2(root, 0, out _);
+    }
+
+    private bool DepthFirstSearch_Ver1(TreeNode root, out int height)
+    {
+        if (root == null)
         {
-            return DepthFirstSearch_BottomUp(root, out _);
+            height = 0;
+            return true;
         }
 
-        public bool IsBalanced_TopDown(TreeNode root)
+        var leftResult = DepthFirstSearch_Ver1(root.left, out var leftHeight);
+        var rightResult = DepthFirstSearch_Ver1(root.right, out var rightHeight);
+
+        height = Math.Max(leftHeight, rightHeight) + 1;
+
+        return leftResult && rightResult && Math.Abs(leftHeight - rightHeight) <= 1;
+    }
+
+    private bool DepthFirstSearch_Ver2(TreeNode root, int depth, out int childDepth)
+    {
+        if (root == null)
         {
-            return DepthFirstSearch_TopDown(root, 0, out _);
+            childDepth = depth;
+            return true;
         }
 
-        private bool DepthFirstSearch_BottomUp(TreeNode root, out int depth)
-        {
-            if (root == null)
-            {
-                depth = 0;
-                return true;
-            }
+        var leftResult = DepthFirstSearch_Ver2(root.left, depth + 1, out var leftDepth);
+        var rightResult = DepthFirstSearch_Ver2(root.right, depth + 1, out var rightDepth);
 
-            var leftResult = DepthFirstSearch_BottomUp(root.left, out var leftDepth);
-            var rightResult = DepthFirstSearch_BottomUp(root.right, out var rightDepth);
+        childDepth = Math.Max(leftDepth, rightDepth);
 
-            depth = Math.Max(leftDepth, rightDepth) + 1;
-
-            return leftResult && rightResult && Math.Abs(leftDepth - rightDepth) <= 1;
-        }
-
-        private bool DepthFirstSearch_TopDown(TreeNode root, int depth, out int childDepth)
-        {
-            if (root == null)
-            {
-                childDepth = depth;
-                return true;
-            }
-
-            var leftResult = DepthFirstSearch_TopDown(root.left, depth + 1, out var leftDepth);
-            var rightResult = DepthFirstSearch_TopDown(root.right, depth + 1, out var rightDepth);
-
-            childDepth = Math.Max(leftDepth, rightDepth);
-
-            return leftResult && rightResult && Math.Abs(leftDepth - rightDepth) <= 1;
-        }
+        return leftResult && rightResult && Math.Abs(leftDepth - rightDepth) <= 1;
     }
 }
--- LeetCodeTests/SolutionTest0101_0200/Solution0110Test.cs
+++ LeetCodeTests/SolutionTest0101_0200/Solution0110Test.cs
@@ -2,51 +2,55 @@
 using LeetCode.Solution0101_0200;
 using NUnit.Framework;
 
-namespace LeetCodeTests.SolutionTest0101_0200
+namespace LeetCodeTests.SolutionTest0101_0200;
+
+[TestFixture]
+[Category("SolutionTest0101_0200")]
+public class Solution0110Test
 {
-    [TestFixture]
-    [Category("SolutionTest0101_0200")]
-    public class Solution0110Test
+    [TestCaseSource(nameof(TestCases))]
+    public void IsBalanced_Ver1Test(int?[] array, bool expected)
     {
-        [TestCaseSource(nameof(TestCases))]
-        public void IsBalanced_BottomUpTest(int?[] array, bool expected)
-        {
-            var root = Utility.BuildTree(array, 0);
+        var root = Utility.BuildTree(array, 0);
 
-            var solution = new Solution0110();
-            var actual = solution.IsBalanced_BottomUp(root);
+        var solution = new Solution0110();
+        var actual = solution.IsBalanced_Ver1(root);
 
-            actual.Should().Be(expected);
-        }
-
-        [TestCaseSource(nameof(TestCases))]
-        public void IsBalanced_TopDownTest(int?[] array, bool expected)
-        {
-            var root = Utility.BuildTree(array, 0);
-
-            var solution = new Solution0110();
-            var actual = solution.IsBalanced_TopDown(root);
-
-            actual.Should().Be(expected);
-        }
-
-        private static readonly object[] TestCases =
-        {
-            new object[]
-            {
-                new int?[] { 3, 9, 20, null, null, 15, 7 },
-                true
-            },
-            new object[]
-            {
-                new int?[] { 1, 2, 2, 3, 3, null, null, 4, 4 },
-                false
-            },
-            new object[]
-            {
-                new int?[] {0 },
-                true
-            }
-        };
+        actual.Should().Be(expected);
     }
+
+    [TestCaseSource(nameof(TestCases))]
+    public void IsBalanced_Ver2Test(int?[] array, bool expected)
+    {
+        var root = Utility.BuildTree(array, 0);
+
+        var solution = new Solution0110();
+        var actual = solution.IsBalanced_Ver2(root);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new int?[] { 3, 9, 20, null, null, 15, 7 },
+            true
+        },
+        new object[]
+        {
+            new int?[] { 1, 2, 2, 3, 3, null, null, 4, 4 },
+            false
+        },
+        new object[]
+        {
+            new int?[] { },
+            true
+        },
+        new object[]
+        {
+            new int?[] { 0 },
+            true
+        }
+    };
 }

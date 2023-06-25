112f710ce15db0827bdbe84c5aa910b842e65447
ThunderstormWang <thunderstormwang@gmail.com>
2023-06-25 17:10:47 +0800

    add problem 236

---
+++ LeetCode/Solution0201_0300/Solution0236.cs
@@ -0,0 +1,76 @@
+﻿using LeetCode.Models;
+
+namespace LeetCode.Solution0201_0300;
+
+public class Solution0236
+{
+    public TreeNode LowestCommonAncestor_Ver1(TreeNode root, TreeNode p, TreeNode q)
+    {
+        var foundP = false;
+        var foundQ = false;
+        return PostorderSearch(root, p, q, out foundP, out foundQ);
+    }
+
+    public TreeNode LowestCommonAncestor_Ver2(TreeNode root, TreeNode p, TreeNode q)
+    {
+        return PrefixSearch(root, p, q);
+    }
+
+    private TreeNode PostorderSearch(TreeNode root, TreeNode p, TreeNode q, out bool foundP, out bool foundQ)
+    {
+        if (root == null)
+        {
+            foundP = false;
+            foundQ = false;
+            return null;
+        }
+
+        var leftFoundP = false;
+        var leftFoundQ = false;
+        var left = PostorderSearch(root.left, p, q, out leftFoundP, out leftFoundQ);
+        if (left != null)
+        {
+            foundP = true;
+            foundQ = true;
+            return left;
+        }
+
+        var rightFoundP = false;
+        var rightFoundQ = false;
+        var right = PostorderSearch(root.right, p, q, out rightFoundP, out rightFoundQ);
+        if (right != null)
+        {
+            foundP = true;
+            foundQ = true;
+            return right;
+        }
+
+        foundP = leftFoundP || rightFoundP || root.val == p.val;
+        foundQ = leftFoundQ || rightFoundQ || root.val == q.val;
+
+        return foundP && foundQ ? root : null;
+    }
+
+    private TreeNode PrefixSearch(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+
+        if (root.val == p.val || root.val == q.val)
+        {
+            return root;
+        }
+
+        var left = PrefixSearch(root.left, p, q);
+        var right = PrefixSearch(root.right, p, q);
+
+        if (left != null && right != null)
+        {
+            return root;
+        }
+
+        return left == null ? right : left;
+    }
+}
---
+++ LeetCodeTests/SolutionTest0201_0300/Solution0236Test.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using LeetCode.Models;
+using LeetCode.Solution0201_0300;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0201_0300;
+
+[TestFixture]
+[Category("SolutionTest0201_0300")]
+public class Solution0236Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void LowestCommonAncestor_Ver1Test(int?[] inputArray, int p, int q, TreeNode expected)
+    {
+        var root = Utility.BuildTree(inputArray, 0);
+        var nodeP = new TreeNode(p);
+        var nodeQ = new TreeNode(q);
+
+        var solution = new Solution0236();
+        var actual = solution.LowestCommonAncestor_Ver1(root, nodeP, nodeQ);
+
+        actual.val.Should().Be(expected.val);
+    }
+    
+    [TestCaseSource(nameof(TestCases))]
+    public void LowestCommonAncestor_Ver2Test(int?[] inputArray, int p, int q, TreeNode expected)
+    {
+        var root = Utility.BuildTree(inputArray, 0);
+        var nodeP = new TreeNode(p);
+        var nodeQ = new TreeNode(q);
+
+        var solution = new Solution0236();
+        var actual = solution.LowestCommonAncestor_Ver2(root, nodeP, nodeQ);
+
+        actual.val.Should().Be(expected.val);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new int?[] { 3, 5, 1, 6, 2, 0, 8, null, null, 7, 4 },
+            5,
+            1,
+            new TreeNode(3)
+        },
+        new object[]
+        {
+            new int?[] { 3, 5, 1, 6, 2, 0, 8, null, null, 7, 4 },
+            5,
+            4,
+            new TreeNode(5)
+        },
+        new object[]
+        {
+            new int?[] { 1, 2 },
+            1,
+            2,
+            new TreeNode(1)
+        }
+    };
+}

a4d5a6b78fef39a25b3dc0ed88202bdc5e6cf859
ThunderstormWang <thunderstormwang@gmail.com>
2023-07-09 17:45:37 +0800

    modify problem 31

--- LeetCode/Solution0001_0050/Solution0031.cs
+++ LeetCode/Solution0001_0050/Solution0031.cs
@@ -1,55 +1,93 @@
-﻿namespace LeetCode.Solution0001_0050
+﻿namespace LeetCode.Solution0001_0050;
+
+public class Solution0031
 {
-    public class Solution0031
+    public void NextPermutation_Ver1(int[] nums)
     {
-        public void NextPermutation(int[] nums)
+        var lastIncrIndex = -1;
+
+        for (var i = 1; i < nums.Length; i++)
         {
-            var decreasingIndex = -1;
-            for (var i = nums.Length - 2; i >= 0; i--)
+            if (nums[i] <= nums[i - 1])
             {
-                if (nums[i] >= nums[i + 1])
+                continue;
+            }
+
+            lastIncrIndex = i;
+        }
+
+        if (lastIncrIndex != -1)
+        {
+            var targetIndex = lastIncrIndex - 1;
+            var candidateIndex = lastIncrIndex;
+
+            for (var i = lastIncrIndex; i < nums.Length; i++)
+            {
+                if (nums[i] <= nums[targetIndex])
                 {
                     continue;
                 }
 
-                decreasingIndex = i;
+                candidateIndex = i;
+            }
+
+            var temp = nums[candidateIndex];
+            nums[candidateIndex] = nums[targetIndex];
+            nums[targetIndex] = temp;
+        }
+
+        var startIndex = lastIncrIndex == -1 ? 0 : lastIncrIndex;
+        var endIndex = nums.Length - 1;
+
+        while (startIndex < endIndex)
+        {
+            var temp = nums[startIndex];
+            nums[startIndex] = nums[endIndex];
+            nums[endIndex] = temp;
+
+            startIndex++;
+            endIndex--;
+        }
+    }
+
+    public void NextPermutation_Ver2(int[] nums)
+    {
+        var decreasingIndex = -1;
+        for (var i = nums.Length - 2; i >= 0; i--)
+        {
+            if (nums[i] >= nums[i + 1])
+            {
+                continue;
+            }
+
+            decreasingIndex = i;
+            break;
+        }
+
+        if (decreasingIndex != -1)
+        {
+            var justLargerIndex = 0;
+            for (var i = nums.Length - 1; i >= 0; i--)
+            {
+                if (nums[i] <= nums[decreasingIndex])
+                {
+                    continue;
+                }
+
+                justLargerIndex = i;
                 break;
             }
 
-            if (decreasingIndex != -1)
-            {
-                var justLargerIndex = 0;
-                for (var i = nums.Length - 1; i >= 0; i--)
-                {
-                    if (nums[i] <= nums[decreasingIndex])
-                    {
-                        continue;
-                    }
-
-                    justLargerIndex = i;
-                    break;
-                }
-
-                (nums[decreasingIndex], nums[justLargerIndex]) = (nums[justLargerIndex], nums[decreasingIndex]);
-            }
-
-            var startIndex = decreasingIndex + 1;
-            var endIndex = nums.Length - 1;
-            while (startIndex < endIndex)
-            {
-                (nums[startIndex], nums[endIndex]) =
-                    (nums[endIndex], nums[startIndex]);
-                startIndex++;
-                endIndex--;
-            }
+            (nums[decreasingIndex], nums[justLargerIndex]) = (nums[justLargerIndex], nums[decreasingIndex]);
         }
-        
-        // 用暴力法
-        // Time: O(N!)
-        // Space: O(N)
-        
-        // 官方解法
-        // Time: O(N)
-        // Space: O(1)
+
+        var startIndex = decreasingIndex + 1;
+        var endIndex = nums.Length - 1;
+        while (startIndex < endIndex)
+        {
+            (nums[startIndex], nums[endIndex]) = (nums[endIndex], nums[startIndex]);
+            startIndex++;
+            endIndex--;
+        }
     }
 }
--- LeetCodeTests/SolutionTest0001_0050/Solution0031Test.cs
+++ LeetCodeTests/SolutionTest0001_0050/Solution0031Test.cs
@@ -2,24 +2,67 @@
 using LeetCode.Solution0001_0050;
 using NUnit.Framework;
 
-namespace LeetCodeTests.SolutionTest0001_0050
-{
-    [TestFixture]
-    [Category("SolutionTest0001_0050")]
-    public class Solution0031Test
-    {
-        [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 3, 2 })]
-        [TestCase(new int[] { 1, 3, 2 }, new int[] { 2, 1, 3 })]
-        [TestCase(new int[] { 2, 1, 3 }, new int[] { 2, 3, 1 })]
-        [TestCase(new int[] { 2, 3, 1 }, new int[] { 3, 1, 2 })]
-        [TestCase(new int[] { 3, 1, 2 }, new int[] { 3, 2, 1 })]
-        [TestCase(new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 })]
-        public void NextPermutationTest(int[] nums, int[] expected)
-        {
-            var solution = new Solution0031();
-            solution.NextPermutation(nums);
+namespace LeetCodeTests.SolutionTest0001_0050;
 
-            nums.Should().Equal(expected);
-        }
+[TestFixture]
+[Category("SolutionTest0001_0050")]
+public class Solution0031Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void NextPermutation_Ver1Test(int[] nums, int[] expected)
+    {
+        var actual = new int [nums.Length];
+        Array.Copy(nums, actual, nums.Length);
+
+        var solution = new Solution0031();
+        solution.NextPermutation_Ver1(actual);
+
+        actual.Should().Equal(expected);
     }
+
+    [TestCaseSource(nameof(TestCases))]
+    public void NextPermutation_Ver2Test(int[] nums, int[] expected)
+    {
+        var actual = new int [nums.Length];
+        Array.Copy(nums, actual, nums.Length);
+
+        var solution = new Solution0031();
+        solution.NextPermutation_Ver2(actual);
+
+        actual.Should().Equal(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new int[] { 1, 2, 3 },
+            new int[] { 1, 3, 2 }
+        },
+        new object[]
+        {
+            new int[] { 1, 3, 2 },
+            new int[] { 2, 1, 3 }
+        },
+        new object[]
+        {
+            new int[] { 2, 1, 3 },
+            new int[] { 2, 3, 1 }
+        },
+        new object[]
+        {
+            new int[] { 2, 3, 1 },
+            new int[] { 3, 1, 2 }
+        },
+        new object[]
+        {
+            new int[] { 3, 1, 2 },
+            new int[] { 3, 2, 1 }
+        },
+        new object[]
+        {
+            new int[] { 3, 2, 1 },
+            new int[] { 1, 2, 3 }
+        },
+    };
 }

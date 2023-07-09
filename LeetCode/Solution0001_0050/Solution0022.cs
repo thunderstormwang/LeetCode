382140362b1211d0738ddb1d5751c6a32682b0bc
ThunderstormWang <thunderstormwang@gmail.com>
2023-07-09 18:32:48 +0800

    modify problem 22

--- LeetCode/Solution0001_0050/Solution0022.cs
+++ LeetCode/Solution0001_0050/Solution0022.cs
@@ -1,117 +1,125 @@
-﻿namespace LeetCode.Solution0001_0050
+﻿using System.Text;
+
+namespace LeetCode.Solution0001_0050;
+
+public class Solution0022
 {
-    public class Solution0022
+    public IList<string> GenerateParenthesis_Ver1(int n)
     {
-        private readonly int[] _parenthesisCandidate = new int[2]
-        {
-            1, -1
-        };
+        var result = new List<string>();
 
-        public IList<string> GenerateParenthesis_Approach1(int n)
-        {
-            var result = new List<string>();
-            FindCombination_PutAndCheck(new int[n * 2], 0, result);
+        Backtrack_Ver1(n, 0, new StringBuilder(), result);
 
-            return result;
+        return result;
+    }
+
+    private void Backtrack_Ver1(int n, int left, StringBuilder curr, IList<string> result)
+    {
+        if (curr.Length == 2 * n)
+        {
+            result.Add(curr.ToString());
         }
 
-        private void FindCombination_PutAndCheck(int[] parenthesisArray, int index, List<string> result)
+        if (left < n)
         {
-            if (index == parenthesisArray.Length)
-            {
-                var temp = string.Join(string.Empty, parenthesisArray.Select(p => p == 1 ? "(" : ")"));
-                result.Add(temp);
-
-                return;
-            }
-
-            foreach (var item in _parenthesisCandidate)
-            {
-                parenthesisArray[index] = item;
-                if (!Check(parenthesisArray,
-                    index))
-                {
-                    continue;
-                }
-
-                FindCombination_PutAndCheck(parenthesisArray,
-                    index + 1,
-                    result);
-            }
+            curr.Append('(');
+            Backtrack_Ver1(n, left + 1, curr, result);
+            curr.Remove(curr.Length - 1, 1);
         }
 
-        private bool Check(int[] parenthesisArray,
-            int index)
+        var right = curr.Length - left;
+        if (right < left)
         {
-            var sum = 0;
-            var upperLimit = parenthesisArray.Length / 2;
+            curr.Append(')');
+            Backtrack_Ver1(n, left, curr, result);
+            curr.Remove(curr.Length - 1, 1);
+        }
+    }
 
-            if (parenthesisArray.Where(p => p > 0).Sum() > upperLimit)
+    public IList<string> GenerateParenthesis_Ver2(int n)
+    {
+        var result = new List<string>();
+        Backtrack_Ver2(new int[n * 2], 0, n, 0, result);
+
+        return result;
+    }
+
+    private void Backtrack_Ver2(int[] parenthesisArray, int index, int open, int close, List<string> result)
+    {
+        if (index == parenthesisArray.Length)
+        {
+            var temp = string.Join(string.Empty, parenthesisArray.Select(p => p == 1 ? "(" : ")"));
+            result.Add(temp);
+
+            return;
+        }
+
+        if (open > 0)
+        {
+            parenthesisArray[index] = 1;
+            Backtrack_Ver2(parenthesisArray, index + 1, open - 1, close + 1, result);
+        }
+
+        if (close > 0)
+        {
+            parenthesisArray[index] = -1;
+            Backtrack_Ver2(parenthesisArray, index + 1, open, close - 1, result);
+        }
+    }
+
+    public IList<string> GenerateParenthesis_Ver3(int n)
+    {
+        var result = new List<string>();
+        var candidates = new int[] { 1, -1 };
+        Backtrack_Ver3(new int[n * 2], 0, candidates, result);
+
+        return result;
+    }
+
+    private void Backtrack_Ver3(int[] parenthesisArray, int index, int[] candidates, List<string> result)
+    {
+        if (index == parenthesisArray.Length)
+        {
+            var temp = string.Join(string.Empty, parenthesisArray.Select(p => p == 1 ? "(" : ")"));
+            result.Add(temp);
+
+            return;
+        }
+
+        foreach (var item in candidates)
+        {
+            parenthesisArray[index] = item;
+            if (!Check(parenthesisArray, index))
+            {
+                continue;
+            }
+
+            Backtrack_Ver3(parenthesisArray, index + 1, candidates, result);
+        }
+    }
+
+    private bool Check(int[] parenthesisArray, int index)
+    {
+        var sum = 0;
+        var upperLimit = parenthesisArray.Length / 2;
+
+        if (parenthesisArray.Where(p => p > 0).Sum() > upperLimit)
+        {
+            return false;
+        }
+
+        for (var i = 0; i <= index; i++)
+        {
+            sum += parenthesisArray[i];
+            if (sum < 0)
             {
                 return false;
             }
-
-            for (var i = 0; i <= index; i++)
-            {
-                sum += parenthesisArray[i];
-                if (sum < 0)
-                {
-                    return false;
-                }
-            }
-
-            return true;
         }
 
-        // Time: O(2*N)
-        // Space: ??
-
-        public IList<string> GenerateParenthesis_Approach2(int n)
-        {
-            var result = new List<string>();
-            FindCombination_AlwaysPutValid(new int[n * 2],
-                0,
-                n,
-                0,
-                result);
-
-            return result;
-        }
-
-        private void FindCombination_AlwaysPutValid(int[] parenthesisArray,
-            int index,
-            int open,
-            int close,
-            List<string> result)
-        {
-            if (index == parenthesisArray.Length)
-            {
-                var temp = string.Join(string.Empty,
-                    parenthesisArray.Select(p => p == 1 ? "(" : ")"));
-                result.Add(temp);
-
-                return;
-            }
-
-            if (open > 0)
-            {
-                parenthesisArray[index] = 1;
-                FindCombination_AlwaysPutValid(parenthesisArray,
-                    index + 1,
-                    open - 1,
-                    close + 1,
-                    result);
-            }
-
-            if (close > 0)
-            {
-                parenthesisArray[index] = -1;
-                FindCombination_AlwaysPutValid(parenthesisArray,
-                    index + 1,
-                    open,
-                    close - 1,
-                    result);
-            }
-        }
+        return true;
     }
+
+    // Time: O(2*N)
+    // Space: ??
 }
--- LeetCodeTests/SolutionTest0001_0050/Solution0022Test.cs
+++ LeetCodeTests/SolutionTest0001_0050/Solution0022Test.cs
@@ -1,41 +1,56 @@
-﻿using System.Collections.Generic;
-using FluentAssertions;
+﻿using FluentAssertions;
 using LeetCode.Solution0001_0050;
 using NUnit.Framework;
 
-namespace LeetCodeTests.SolutionTest0001_0050
+namespace LeetCodeTests.SolutionTest0001_0050;
+
+[TestFixture]
+[Category("SolutionTest0001_0050")]
+public class Solution0022Test
 {
-    [TestFixture]
-    [Category("SolutionTest0001_0050")]
-    public class Solution0022Test
+    [TestCaseSource(nameof(TestCases))]
+    public void GenerateParenthesis_Ver1Test(int input, string[] expected)
     {
-        [TestCase(1, new string[] { "()" })]
-        [TestCase(2, new string[] { "(())" ,"()()"})]
-        [TestCase(3, new string[] { "((()))", "(()())", "(())()", "()(())", "()()()" })]
-        public void ThreeSumClosestTest(int input, string[] expected)
-        {
-            var solution = new Solution0022();
+        var solution = new Solution0022();
+        var actual = solution.GenerateParenthesis_Ver1(input);
 
-            var actual = solution.GenerateParenthesis_Approach1(input);
-
-            var expectedList = new List<string>(expected);
-
-            actual.Should().BeEquivalentTo(expectedList);
-        }
-        
-        [TestCase(1, new string[] { "()" })]
-        [TestCase(2, new string[] { "(())" ,"()()"})]
-        [TestCase(3, new string[] { "((()))", "(()())", "(())()", "()(())", "()()()" })]
-        public void ThreeSumClosestTest_Approach2(int input,
-            string[] expected)
-        {
-            var solution = new Solution0022();
-
-            var actual = solution.GenerateParenthesis_Approach2(input);
-
-            var expectedList = new List<string>(expected);
-
-            actual.Should().BeEquivalentTo(expectedList);
-        }
+        actual.Should().BeEquivalentTo(expected);
     }
+    
+    [TestCaseSource(nameof(TestCases))]
+    public void GenerateParenthesis_Ver2Test(int input, string[] expected)
+    {
+        var solution = new Solution0022();
+        var actual = solution.GenerateParenthesis_Ver3(input);
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public void GenerateParenthesis_Ver3Test(int input, string[] expected)
+    {
+        var solution = new Solution0022();
+        var actual = solution.GenerateParenthesis_Ver2(input);
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            1,
+            new string[] { "()" }
+        },
+        new object[]
+        {
+            2,
+            new string[] { "(())", "()()" }
+        },
+        new object[]
+        {
+            3,
+            new string[] { "((()))", "(()())", "(())()", "()(())", "()()()" }
+        },
+    };
 }

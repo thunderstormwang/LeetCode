373c1c43c9e405b7739b18e0b760af2118ef0ada
ThunderstormWang <thunderstormwang@gmail.com>
2023-07-10 21:05:39 +0800

    modify problem 19

--- LeetCode/Solution0001_0050/Solution0019.cs
+++ LeetCode/Solution0001_0050/Solution0019.cs
@@ -1,37 +1,56 @@
 ﻿using LeetCode.Models;
 
-namespace LeetCode.Solution0001_0050
+namespace LeetCode.Solution0001_0050;
+
+public class Solution0019
 {
-    public class Solution0019
+    public ListNode RemoveNthFromEnd_Ver1(ListNode head, int n)
     {
-        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        var dummyNode = new ListNode(0);
+        dummyNode.next = head;
+        var fast = dummyNode;
+        var slow = dummyNode;
+
+        var count = n + 1;
+        while (count > 0)
         {
-            var dummyNode = new ListNode(0);
-            dummyNode.next = head;
-            var fast = dummyNode;
-            var slow = dummyNode;
-
-            var index = 0;
-            while (index < n)
-            {
-                fast = fast.next;
-                index++;
-            }
-
-            while (fast.next != null)
-            {
-                fast = fast.next;
-                slow = slow.next;
-            }
-
-            slow.next = slow.next.next;
-
-            return dummyNode.next;
+            fast = fast.next;
+            count--;
         }
-        
-        // 這題考你能否找到在一次 traverse 就做完的解法
-        
-        // Time: O(N)
-        // Space: O(1)
+
+        while (fast != null)
+        {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummyNode.next;
+    }
+
+    public ListNode RemoveNthFromEnd_Ver2(ListNode head, int n)
+    {
+        var dummyNode = new ListNode(0);
+        dummyNode.next = head;
+        var fast = dummyNode;
+        var slow = dummyNode;
+
+        var count = n;
+        while (count > 0)
+        {
+            fast = fast.next;
+            count--;
+        }
+
+        while (fast.next != null)
+        {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummyNode.next;
     }
 }
--- LeetCodeTests/SolutionTest0001_0050/Solution0019Test.cs
+++ LeetCodeTests/SolutionTest0001_0050/Solution0019Test.cs
@@ -1,28 +1,80 @@
-﻿using LeetCode.Solution0001_0050;
+﻿using FluentAssertions;
+using LeetCode.Solution0001_0050;
 using NUnit.Framework;
 
-namespace LeetCodeTests.SolutionTest0001_0050
+namespace LeetCodeTests.SolutionTest0001_0050;
+
+[TestFixture]
+[Category("SolutionTest0001_0050")]
+public class Solution0019Test
 {
-    [TestFixture]
-    [Category("SolutionTest0001_0050")]
-    public class Solution0019Test
+    [TestCaseSource(nameof(TestCases))]
+    public void RemoveNthFromEnd_Ver1Test(int[] input, int n, int[] expected)
     {
-        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 1, new int[] { 1, 2, 3, 4 })]
-        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 2, new int[] { 1, 2, 3, 5 })]
-        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 3, new int[] { 1, 2, 4, 5 })]
-        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 4, new int[] { 1, 3, 4, 5 })]
-        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 5, new int[] { 2, 3, 4, 5 })]
-        [TestCase(new int[] { 1 }, 1, new int[] { })]
-        [TestCase(new int[] { 1, 2 }, 1, new int[] { 1 })]
-        public void RemoveNthFromEndTest(int[] input, int n, int[] expected)
-        {
-            var solution = new Solution0019();
+        var list = Utility.BuildListNode(input);
+        var expectedListNode = Utility.BuildListNode(expected);
 
-            var list = Utility.BuildListNode(input);
-            var actual = solution.RemoveNthFromEnd(list, n);
+        var solution = new Solution0019();
+        var actual = solution.RemoveNthFromEnd_Ver1(list, n);
 
-            var expectedListNode = Utility.BuildListNode(expected);
-            Assert.That(Utility.CheckLinkedList(actual, expectedListNode), Is.EqualTo(true));
-        }
+        Utility.CheckLinkedList(actual, expectedListNode).Should().BeTrue();
     }
+    
+    [TestCaseSource(nameof(TestCases))]
+    public void RemoveNthFromEnd_Ver2Test(int[] input, int n, int[] expected)
+    {
+        var list = Utility.BuildListNode(input);
+        var expectedListNode = Utility.BuildListNode(expected);
+
+        var solution = new Solution0019();
+        var actual = solution.RemoveNthFromEnd_Ver2(list, n);
+
+        Utility.CheckLinkedList(actual, expectedListNode).Should().BeTrue();
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new int[] { 1, 2, 3, 4, 5 },
+            1,
+            new int[] { 1, 2, 3, 4 }
+        },
+        new object[]
+        {
+            new int[] { 1, 2, 3, 4, 5 },
+            2,
+            new int[] { 1, 2, 3, 5 }
+        },
+        new object[]
+        {
+            new int[] { 1, 2, 3, 4, 5 },
+            3,
+            new int[] { 1, 2, 4, 5 }
+        },
+        new object[]
+        {
+            new int[] { 1, 2, 3, 4, 5 },
+            4,
+            new int[] { 1, 3, 4, 5 }
+        },
+        new object[]
+        {
+            new int[] { 1, 2, 3, 4, 5 },
+            5,
+            new int[] { 2, 3, 4, 5 }
+        },
+        new object[]
+        {
+            new int[] { 1 },
+            1,
+            new int[] { }
+        },
+        new object[]
+        {
+            new int[] { 1, 2 },
+            1,
+            new int[] { 1 }
+        },
+    };
 }

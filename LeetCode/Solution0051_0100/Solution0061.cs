8f9793fbbb352ce2ce2162e353db318a34595a5f
ThunderstormWang <thunderstormwang@gmail.com>
2023-07-10 22:52:11 +0800

    modify problem 61

---
+++ LeetCode/Solution0051_0100/Solution0061.cs
@@ -0,0 +1,110 @@
+﻿using LeetCode.Models;
+
+namespace LeetCode.Solution0051_0100;
+
+public class Solution0061
+{
+    public ListNode RotateRight_Ver1(ListNode head, int k)
+    {
+        if (head == null)
+        {
+            return null;
+        }
+
+        var dummyNode = new ListNode(0);
+        dummyNode.next = head;
+        head = dummyNode;
+        var n = 0;
+
+        while (head.next != null)
+        {
+            head = head.next;
+            n++;
+        }
+
+        k %= n;
+
+        if (k == 0)
+        {
+            return dummyNode.next;
+        }
+
+        var count = k + 1;
+        var fast = dummyNode;
+        var slow = dummyNode;
+
+        while (count > 0)
+        {
+            fast = fast.next;
+            count--;
+        }
+
+        while (fast != null)
+        {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        var temp = slow.next;
+        slow.next = null;
+        slow = temp;
+
+        while (slow.next != null)
+        {
+            slow = slow.next;
+        }
+
+        slow.next = dummyNode.next;
+        dummyNode.next = temp;
+
+        return dummyNode.next;
+    }
+
+    public ListNode RotateRight_Ver2(ListNode head, int k)
+    {
+        if (head == null)
+        {
+            return null;
+        }
+
+        var dummyNode = new ListNode(0);
+        dummyNode.next = head;
+        head = dummyNode;
+        var n = 0;
+
+        while (head.next != null)
+        {
+            head = head.next;
+            n++;
+        }
+
+        k %= n;
+
+        if (k == 0)
+        {
+            return dummyNode.next;
+        }
+
+        var count = k;
+        var fast = dummyNode;
+        var slow = dummyNode;
+
+        while (count > 0)
+        {
+            fast = fast.next;
+            count--;
+        }
+
+        while (fast.next != null)
+        {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        fast.next = dummyNode.next;
+        dummyNode.next = slow.next;
+        slow.next = null;
+
+        return dummyNode.next;
+    }
+}
---
+++ LeetCodeTests/SolutionTest0051_0100/Solution0061Test.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0051_0100;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0051_0100;
+
+[TestFixture]
+[Category("SolutionTest0051_0100")]
+public class Solution0061Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void RotateRight_Ver1Test(int[] input, int n, int[] expected)
+    {
+        var list = Utility.BuildListNode(input);
+        var expectedListNode = Utility.BuildListNode(expected);
+
+        var solution = new Solution0061();
+        var actual = solution.RotateRight_Ver1(list, n);
+
+        Utility.CheckLinkedList(actual, expectedListNode).Should().BeTrue();
+    }
+    
+    [TestCaseSource(nameof(TestCases))]
+    public void RotateRight_Ver2Test(int[] input, int n, int[] expected)
+    {
+        var list = Utility.BuildListNode(input);
+        var expectedListNode = Utility.BuildListNode(expected);
+
+        var solution = new Solution0061();
+        var actual = solution.RotateRight_Ver2(list, n);
+
+        Utility.CheckLinkedList(actual, expectedListNode).Should().BeTrue();
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new int[] { 0, 1, 2 },
+            0,
+            new int[] { 0, 1, 2 }
+        },
+        new object[]
+        {
+            new int[] { 0, 1, 2 },
+            1,
+            new int[] { 2, 0, 1 }
+        },
+        new object[]
+        {
+            new int[] { 0, 1, 2 },
+            2,
+            new int[] { 1, 2, 0 }
+        },
+        new object[]
+        {
+            new int[] { 0, 1, 2 },
+            3,
+            new int[] { 0, 1, 2 }
+        },
+        new object[]
+        {
+            new int[] { 0, 1, 2 },
+            4,
+            new int[] { 2, 0, 1 }
+        },
+        new object[]
+        {
+            new int[] { 0, 1, 2 },
+            5,
+            new int[] { 1, 2, 0 }
+        },
+        new object[]
+        {
+            new int[] { 0, 1, 2 },
+            6,
+            new int[] { 0, 1, 2 }
+        },
+        new object[]
+        {
+            new int[] { 1, 2, 3, 4, 5 },
+            2,
+            new int[] { 4, 5, 1, 2, 3 }
+        }
+    };
+}

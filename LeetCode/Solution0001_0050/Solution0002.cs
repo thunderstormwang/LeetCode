1d4ffc6e704ceaf671e046f55e7c7fea7cecc4cc
ThunderstormWang <thunderstormwang@gmail.com>
2023-07-11 21:29:12 +0800

    modify problem 2

--- LeetCode/Solution0001_0050/Solution0002.cs
+++ LeetCode/Solution0001_0050/Solution0002.cs
@@ -1,39 +1,80 @@
 ﻿using LeetCode.Models;
 
-namespace LeetCode.Solution0001_0050
+namespace LeetCode.Solution0001_0050;
+
+public class Solution0002
 {
-    public class Solution0002
+    public ListNode AddTwoNumbers_Ver1(ListNode l1, ListNode l2)
     {
-        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        var dummyNode = new ListNode(0);
+        var carry = 0;
+        var curr = dummyNode;
+
+        while (l1 != null && l2 != null)
         {
-            var dummyNode = new ListNode(0);
-            var temp = dummyNode;
-            var carry = 0;
+            var temp = l1.val + l2.val + carry;
+            carry = temp / 10 >= 1 ? 1 : 0;
+            var node = new ListNode(temp % 10);
 
-            while (l1 != null || l2 != null)
-            {
-                var x = l1?.val ?? 0;
-                var y = l2?.val ?? 0;
-
-                var sum = x + y + carry;
-                temp.next = new ListNode(sum % 10);
-                carry = sum / 10;
-                temp = temp.next;
-
-                l1 = l1 != null ? l1.next : l1;
-                l2 = l2 != null ? l2.next : l2;
-            }
-
-            if (carry > 0)
-            {
-                temp.next = new ListNode(1);
-            }
-
-            return dummyNode.next;
+            curr.next = node;
+            curr = node;
+            l1 = l1.next;
+            l2 = l2.next;
         }
-        
-        // 注意最後是否需要進位
-        // Time: O(max(l1,l2))
-        // Space: 
+
+        while (l1 != null)
+        {
+            var temp = l1.val + carry;
+            carry = temp / 10 >= 1 ? 1 : 0;
+            var node = new ListNode(temp % 10);
+
+            curr.next = node;
+            curr = node;
+            l1 = l1.next;
+        }
+
+        while (l2 != null)
+        {
+            var temp = l2.val + carry;
+            carry = temp / 10 >= 1 ? 1 : 0;
+            var node = new ListNode(temp % 10);
+
+            curr.next = node;
+            curr = node;
+            l2 = l2.next;
+        }
+
+        if (carry > 0)
+        {
+            var node = new ListNode(1);
+
+            curr.next = node;
+            curr = node;
+        }
+
+        return dummyNode.next;
+    }
+
+    public ListNode AddTwoNumbers_Ver2(ListNode l1, ListNode l2)
+    {
+        var dummyNode = new ListNode(0);
+        var curr = dummyNode;
+        var carry = 0;
+
+        while (l1 != null || l2 != null || carry > 0)
+        {
+            var x = l1?.val ?? 0;
+            var y = l2?.val ?? 0;
+
+            var sum = x + y + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            l1 = l1 != null ? l1.next : l1;
+            l2 = l2 != null ? l2.next : l2;
+        }
+
+        return dummyNode.next;
     }
 }
--- LeetCodeTests/SolutionTest0001_0050/Solution0002Test.cs
+++ LeetCodeTests/SolutionTest0001_0050/Solution0002Test.cs
@@ -2,25 +2,51 @@
 using LeetCode.Solution0001_0050;
 using NUnit.Framework;
 
-namespace LeetCodeTests.SolutionTest0001_0050
+namespace LeetCodeTests.SolutionTest0001_0050;
+
+[TestFixture]
+[Category("SolutionTest0001_0050")]
+public class Solution0002Test
 {
-    [TestFixture]
-    [Category("SolutionTest0001_0050")]
-    public class Solution0002Test
+    [TestCaseSource(nameof(TestCases))]
+    public void AddTwoNumbers_Ver1Test(int[] array1, int[] array2, int[] expectedArray)
     {
-        [TestCase(new int[] { 2, 4, 3 }, new int[] { 5, 6, 4 }, new int[] { 7, 0, 8 })]
-        [TestCase(new int[] { 0 }, new int[] { 0 }, new int[] { 0 })]
-        [TestCase(new int[] { 9, 9, 9, 9, 9, 9, 9 }, new int[] { 9, 9, 9, 9 }, new int[] { 8, 9, 9, 9, 0, 0, 0, 1 })]
-        public void AddTwoNumbersTest(int[] array1, int[] array2, int[] expectedArray)
-        {
-            var solution = new Solution0002();
-            var list1 = Utility.BuildListNode(array1);
-            var list2 = Utility.BuildListNode(array2);
+        var list1 = Utility.BuildListNode(array1);
+        var list2 = Utility.BuildListNode(array2);
+        var expected = Utility.BuildListNode(expectedArray);
 
-            var actual = solution.AddTwoNumbers(list1, list2);
-            var expected = Utility.BuildListNode(expectedArray);
+        var solution = new Solution0002();
+        var actual = solution.AddTwoNumbers_Ver1(list1, list2);
 
-            Utility.CheckLinkedList(actual, expected).Should().BeTrue();
-        }
+        Utility.CheckLinkedList(actual, expected).Should().BeTrue();
     }
+    
+    [TestCaseSource(nameof(TestCases))]
+    public void AddTwoNumbers_Ver2Test(int[] array1, int[] array2, int[] expectedArray)
+    {
+        var list1 = Utility.BuildListNode(array1);
+        var list2 = Utility.BuildListNode(array2);
+        var expected = Utility.BuildListNode(expectedArray);
+
+        var solution = new Solution0002();
+        var actual = solution.AddTwoNumbers_Ver2(list1, list2);
+
+        Utility.CheckLinkedList(actual, expected).Should().BeTrue();
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new int[] { 2, 4, 3 }, new int[] { 5, 6, 4 }, new int[] { 7, 0, 8 }
+        },
+        new object[]
+        {
+            new int[] { 0 }, new int[] { 0 }, new int[] { 0 }
+        },
+        new object[]
+        {
+            new int[] { 9, 9, 9, 9, 9, 9, 9 }, new int[] { 9, 9, 9, 9 }, new int[] { 8, 9, 9, 9, 0, 0, 0, 1 }
+        }
+    };
 }

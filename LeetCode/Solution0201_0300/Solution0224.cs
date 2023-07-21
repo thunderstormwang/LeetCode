7da78d92b456d6c4b73ccf9f2df7f8c87aba09be
ThunderstormWang <thunderstormwang@gmail.com>
2023-07-21 21:21:34 +0800

    add problem 224

---
+++ LeetCode/Solution0201_0300/Solution0224.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCode.Solution0201_0300;
+
+public class Solution0224
+{
+    public int Calculate(string s)
+    {
+        var result = 0;
+        var sign = 1;
+        var stack = new Stack<int>();
+        var index = 0;
+
+        while (index < s.Length)
+        {
+            if (s[index] >= '0' && s[index] <= '9')
+            {
+                var i = index;
+
+                while (i < s.Length && s[i] >= '0' && s[i] <= '9')
+                {
+                    i++;
+                }
+
+                var temp = int.Parse(s.Substring(index, i - index));
+                result += sign * temp;
+
+                index = i;
+                continue;
+            }
+
+            if (s[index] == '+')
+            {
+                sign = 1;
+            }
+
+            if (s[index] == '-')
+            {
+                sign = -1;
+            }
+
+            if (s[index] == '(')
+            {
+                stack.Push(result);
+                stack.Push(sign);
+                result = 0;
+                sign = 1;
+            }
+
+            if (s[index] == ')')
+            {
+                result *= stack.Pop();
+                result += stack.Pop();
+            }
+
+            index++;
+        }
+
+
+        return result;
+    }
+}
---
+++ LeetCodeTests/SolutionTest0201_0300/Solution0224Test.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0201_0300;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0201_0300;
+
+[TestFixture]
+[Category("SolutionTest0201_0300")]
+public class Solution0224Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void CalculateTest(string s, int expected)
+    {
+        var solution = new Solution0224();
+        var actual = solution.Calculate(s);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            "1 + 1",
+            2
+        },
+        new object[]
+        {
+            " 2-1 + 2 ",
+            3
+        },
+        new object[]
+        {
+            "(1+(4+5+2)-3)+(6+8)",
+            23
+        },
+        new object[]
+        {
+            "(10 - (3 + (3+4)))",
+            0
+        }
+    };
+}

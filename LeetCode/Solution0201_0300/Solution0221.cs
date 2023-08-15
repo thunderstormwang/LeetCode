c78a3740dc2e5854d97c0fd3a72ae118fab3291e
ThunderstormWang <thunderstormwang@gmail.com>
2023-08-15 22:40:56 +0800

    add problem 221

---
+++ LeetCode/Solution0201_0300/Solution0221.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Solution0201_0300;
+
+public class Solution0221
+{
+    public int MaximalSquare(char[][] matrix)
+    {
+        var result = 0;
+        var dp = new int [matrix.Length][];
+        for (var i = 0; i < matrix.Length; i++)
+        {
+            dp[i] = new int [matrix[i].Length];
+        }
+
+        for (var i = 0; i < matrix.Length; i++)
+        {
+            for (var j = 0; j < matrix[i].Length; j++)
+            {
+                dp[i][j] = matrix[i][j] == '1' ? 1 : 0;
+            }
+        }
+
+        for (var i = 0; i < matrix.Length; i++)
+        {
+            for (var j = 0; j < matrix[i].Length; j++)
+            {
+                if (dp[i][j] == 0)
+                {
+                    continue;
+                }
+
+                if (i - 1 >= 0 && j - 1 >= 0)
+                {
+                    dp[i][j] = Math.Min(dp[i - 1][j - 1], Math.Min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+
+                result = Math.Max(result, dp[i][j] * dp[i][j]);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ LeetCodeTests/SolutionTest0201_0300/Solution0221Test.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0201_0300;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0201_0300;
+
+[TestFixture]
+[Category("SolutionTest0201_0300")]
+public class Solution0221Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void FindOrder_Ver2Test(char[][] matrix, int expected)
+    {
+        var solution = new Solution0221();
+
+        var actual = solution.MaximalSquare(matrix);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            new char[][]
+            {
+                new char[] { '1', '0', '1', '0', '0' },
+                new char[] { '1', '0', '1', '1', '1' },
+                new char[] { '1', '1', '1', '1', '1' },
+                new char[] { '1', '0', '0', '1', '0' }
+            },
+            4
+        },
+        new object[]
+        {
+            new char[][]
+            {
+                new char[] { '0', '1' },
+                new char[] { '1', '0' }
+            },
+            1
+        },
+        new object[]
+        {
+            new char[][]
+            {
+                new char[] { '0' }
+            },
+            0
+        }
+    };
+}

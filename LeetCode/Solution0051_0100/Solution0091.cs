119d6a492a4305b6311c75c7673c289e96896000
ThunderstormWang <thunderstormwang@gmail.com>
2023-08-17 23:45:36 +0800

    add problem 91

---
+++ LeetCode/Solution0051_0100/Solution0091.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Solution0051_0100;
+
+public class Solution0091
+{
+    public int NumDecodings(string s)
+    {
+        if (s.Length == 1)
+        {
+            return s[0] == '0' ? 0 : 1;
+        }
+
+        var list = new List<string>
+        {
+            "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+            "20", "21", "22", "23", "24", "25", "26"
+        };
+
+        var dp = new int [s.Length + 1];
+        dp[0] = 1;
+        dp[1] = s[0] == '0' ? 0 : 1;
+
+        for (var i = 2; i < s.Length + 1; i++)
+        {
+            if (s[i - 1] >= '1')
+            {
+                dp[i] += dp[i - 1];
+            }
+
+            var str = s.Substring(i - 2, 2);
+            if (list.Contains(str))
+            {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[s.Length];
+    }
+}
---
+++ LeetCodeTests/SolutionTest0051_0100/Solution0091Test.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0051_0100;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0051_0100;
+
+[TestFixture]
+[Category("SolutionTest0051_0100")]
+public class Solution0091Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void NumDecodingsTest(string s, int expected)
+    {
+        var solution = new Solution0091();
+
+        var actual = solution.NumDecodings(s);
+
+        actual.Should().Be(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            "12",
+            2
+        },
+        new object[]
+        {
+            "226",
+            3
+        },
+        new object[]
+        {
+            "06",
+            0
+        },
+        new object[]
+        {
+            "2101",
+            1
+        },
+        new object[]
+        {
+            "1123",
+            5
+        },
+        new object[]
+        {
+            "10011",
+            0
+        },
+        new object[]
+        {
+            "230",
+            0
+        }
+    };
+}

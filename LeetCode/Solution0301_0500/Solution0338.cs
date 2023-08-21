446814fedd19e47b8a24bf8b188c7819f340659e
ThunderstormWang <thunderstormwang@gmail.com>
2023-08-21 22:18:10 +0800

    add problem 338

---
+++ LeetCode/Solution0301_0500/Solution0338.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode.Solution0301_0500;
+
+public class Solution0338
+{
+    public int[] CountBits_Ver1(int n)
+    {
+        var result = new int [n + 1];
+
+        for (var i = 1; i <= n; i++)
+        {
+            var num = i;
+            var count = 0;
+
+            while (num != 0)
+            {
+                if (num % 2 == 1)
+                {
+                    count++;
+                }
+
+                num /= 2;
+            }
+
+            result[i] = count;
+        }
+
+        return result;
+    }
+
+    public int[] CountBits_Ver2(int n)
+    {
+        var dp = new int [n + 1];
+
+        for (var i = 1; i <= n; i++)
+        {
+            dp[i] = dp[i / 2] + i % 2;
+        }
+
+        return dp;
+    }
+
+    public int[] CountBits_Ver3(int n)
+    {
+        var dp = new int [n + 1];
+
+        for (var i = 1; i <= n; i++)
+        {
+            dp[i] = dp[i & (i - 1)] + 1;
+        }
+
+        return dp;
+    }
+}
---
+++ LeetCodeTests/SolutionTest0301_0500/Solution0338Test.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using LeetCode.Solution0301_0500;
+using NUnit.Framework;
+
+namespace LeetCodeTests.SolutionTest0301_0500;
+
+[TestFixture]
+[Category("SolutionTest0301_0500")]
+public class Solution0338Test
+{
+    [TestCaseSource(nameof(TestCases))]
+    public void CountBits_Ver1Test(int n, int[] expected)
+    {
+        var solution = new Solution0338();
+
+        var actual = solution.CountBits_Ver1(n);
+
+        actual.Should().Equal(expected);
+    }
+    
+    [TestCaseSource(nameof(TestCases))]
+    public void CountBits_Ver2Test(int n, int[] expected)
+    {
+        var solution = new Solution0338();
+
+        var actual = solution.CountBits_Ver2(n);
+
+        actual.Should().Equal(expected);
+    }
+    
+    [TestCaseSource(nameof(TestCases))]
+    public void CountBits_Ver3Test(int n, int[] expected)
+    {
+        var solution = new Solution0338();
+
+        var actual = solution.CountBits_Ver3(n);
+
+        actual.Should().Equal(expected);
+    }
+
+    private static readonly object[] TestCases =
+    {
+        new object[]
+        {
+            2,
+            new int[] { 0, 1, 1 }
+        },
+        new object[]
+        {
+            5,
+            new int[] { 0, 1, 1, 2, 1, 2 }
+        }
+    };
+}
